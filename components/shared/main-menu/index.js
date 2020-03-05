749c39944b530925c92d49551144d20d9d0b2680
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-05 15:03:58 -0600

    [#FZC-l84] #comment Change background color to sidebar

--- components/shared/main-menu/index.js
+++ components/shared/main-menu/index.js
@@ -1,9 +1,10 @@
+import "./style.sass"
 import { Menu } from "antd";
 import { InboxOutlined } from "@ant-design/icons"
 
 export function MainMenu() {
   return (
-    <Menu theme="dark">
+    <Menu theme="dark" className="fico main-menu">
       <Menu.Item>
         <InboxOutlined />
         <span>Perfil de implementadora</span>
