982f9e2c9e5aa4857b31cd21bf4837eef77240d4
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-05 14:52:26 -0600

    [#FZC-l84] #comment Implement toolbar component

--- components/shared/layout/index.js
+++ components/shared/layout/index.js
@@ -1,15 +1,17 @@
 import "./style.sass"
 import { Layout as ALayout } from "antd"
 import { MainMenu } from "../main-menu"
+import { Toolbar } from "../toolbar"
 
 export function Layout({children}) {
   return (
-    <div>
-      <ALayout>
+    <div className="fico layout full-height">
+      <ALayout className="full-height">
         <ALayout.Sider>
           <MainMenu />
         </ALayout.Sider>
         <ALayout>
+          <ALayout.Header><Toolbar /></ALayout.Header>
           <ALayout.Content>{children}</ALayout.Content>
         </ALayout>
       </ALayout>
--- pages/implementer/profile/index.js
+++ pages/implementer/profile/index.js
@@ -2,6 +2,6 @@ import { Layout } from "../../../components/shared";
 
 export default function Profile() {
   return (
-    <Layout><span>Prueba</span></Layout>
+    <Layout><div>Prueba</div></Layout>
   );
 }
