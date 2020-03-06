dcdd7ddbab176071d5054d815c90065f7237f3b2
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-06 12:08:08 -0600

    [#FZC-l84] #comment Load local env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 const withSass = require("@zeit/next-sass")
 const withCSS = require("@zeit/next-css")
+let options = {}
 
-module.exports = withSass(withCSS())
+if(process.env.NODE_ENV !== "production") {
+  const webpack = require("webpack")
+  const { parsed: localEnv } = require("dotenv").config()
+  options = {
+    webpack: (config) => {
+      config.plugins.push(
+        new webpack.EnvironmentPlugin(localEnv)
+      )
+
+      return config
+    }
+  }
+}
+
+module.exports = withSass(withCSS(options))
