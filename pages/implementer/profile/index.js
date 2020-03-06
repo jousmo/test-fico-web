65395f69ff1121c525e6d362a9e52161ed3ee0c7
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-05 18:18:45 -0600

    [#FZC-l84] #comment Implemente general information in profile component

--- pages/implementer/profile/index.js
+++ pages/implementer/profile/index.js
@@ -1,11 +1,15 @@
 import { Layout } from "../../../components/shared"
 import { PageContext } from "../../../contexts/page"
 import { context } from "./context"
+import { GeneralInformation } from "../../../components/implementer/profile"
+import { Row, Col } from "antd"
 
 export default function Profile() {
   return (
     <PageContext.Provider value={context}>
-      <Layout><div>Prueba</div></Layout>
+      <Layout>
+        <GeneralInformation />
+      </Layout>
     </PageContext.Provider>
   )
 }
