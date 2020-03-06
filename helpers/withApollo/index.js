9a1f00a59eba02f4e053ffef17a34c4fc36ffbda
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-06 12:14:32 -0600

    [#FZC-l84] #comment Created withApollo HOC

---
+++ helpers/withApollo/index.js
@@ -0,0 +1,29 @@
+import { ApolloClient } from "apollo-boost"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { createHttpLink } from "apollo-link-http"
+import fetch from "node-fetch"
+import { setContext } from "apollo-link-context"
+
+const link = createHttpLink({ uri: process.env.GRAPHQL_URI, fetch: fetch })
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache()
+})
+
+export function withApollo(Component) {
+  return function(props) {
+    return <Component {...props} client={client} />
+  }
+}
