f852746d6ef0d6af221798afc467788a4a8538c9
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-06 18:31:36 -0600

    [#FZC-I84] #comment Implement ImplementerProfileContext

--- pages/implementer/profile/index.js
+++ pages/implementer/profile/index.js
@@ -2,13 +2,19 @@ import { Layout } from "../../../components/shared"
 import { PageContext } from "../../../contexts/page"
 import { GeneralInformation } from "../../../components/implementer/profile"
 import { data } from "../../../contexts/implementer/profile"
+import { useReducer } from "react"
+import { reducer } from "../../../contexts/implementer/profile/reducer"
 
 export default function Profile() {
+  const [state, dispatch] = useReducer(reducer)
+
   return (
     <PageContext.Provider value={data}>
-      <Layout>
-        <GeneralInformation />
-      </Layout>
+      <ImplementerProfileContext.Provider value={dispatch}>
+        <Layout>
+          <GeneralInformation />
+        </Layout>
+      </ImplementerProfileContext.Provider>
     </PageContext.Provider>
   )
 }
