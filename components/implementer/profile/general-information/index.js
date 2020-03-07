7ab89b1fc54ec96f0978df58e39abd9b56399379
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-06 18:40:19 -0600

    [#FZC-I84] #comment Consume context in general information form

--- components/implementer/profile/general-information/index.js
+++ components/implementer/profile/general-information/index.js
@@ -1,7 +1,15 @@
 import { Card, Select, Form, Row, Col, Input } from "antd";
 import { Section } from "../../../shared";
+import { useContext } from "react";
+import { ImplementerProfileContext } from "../../../../contexts/implementer/profile";
 
 export function GeneralInformation() {
+  const { state, dispatch } = useContext(ImplementerProfileContext)
+
+  const onChange = (value, event) => {
+    console.log(value, event)
+  }
+
   return (
     <Section title="1. Información general">
       <Form
@@ -12,7 +20,7 @@ export function GeneralInformation() {
             <Form.Item
               style={{display: "inline"}}
               label="Tipo de la implementadora">
-              <Select>
+              <Select onChange={onChange}>
                 <Select.Option value="1">Test</Select.Option>
               </Select>
             </Form.Item>
@@ -21,7 +29,7 @@ export function GeneralInformation() {
             <Form.Item
               style={{display: "inline", visibility: "hidden"}}
               label="Tipo de la implementadora">
-              <Select>
+              <Select onChange={onChange}>
                 <Select.Option value="1">Test</Select.Option>
               </Select>
             </Form.Item>
@@ -30,119 +38,119 @@ export function GeneralInformation() {
             <Form.Item
               style={{display: "inline"}}
               label="Nombre de la implementadora">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Director">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="RFC">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Nombre comercial">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Domicilio comercial">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Domicilio fiscal">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Teléfono">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Representante legal">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Correo electrónico">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item
               style={{display: "inline"}}
               label="Oficio de donataria">
-              <Input type="text" />
+              <Input onChange={onChange} type="text" />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Misión">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Visión">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Historia">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Experiencia institucional">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Apoyos anteriores">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Alianzas">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
               style={{display: "inline"}}
               label="Ingresos / egresos">
-              <Input.TextArea autoSize={{minRows: 3}} />
+              <Input.TextArea onChange={onChange} autoSize={{minRows: 3}} />
             </Form.Item>
           </Col>
         </Row>
