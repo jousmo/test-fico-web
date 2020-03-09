bf09860908c1d75e509eea870d326702e518c759
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-09 17:23:33 -0600

    [#FZC-I84] #comment Removed spanish locale because not working properly

--- antd.config.js
+++ antd.config.js
@@ -1,10 +1,7 @@
-import esES from "antd/es/locale/es_ES"
-
 export const antdConfig = {
   form: {
     validateMessages: {
       required: "Es requerido llenar este campo"
     }
-  },
-  locale: esES
+  }
 }
