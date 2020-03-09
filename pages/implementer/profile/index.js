3a5239c6116f04c9369550bbd7bf29952d108693
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-09 14:02:53 -0600

    [#FZC-I84] #comment Implement action context pattern

--- pages/implementer/profile/index.js
+++ pages/implementer/profile/index.js
@@ -2,15 +2,30 @@ import { Layout } from "../../../components/shared"
 import { PageContext } from "../../../contexts/page"
 import { GeneralInformation } from "../../../components/implementer/profile"
 import { data, ImplementerProfileContext } from "../../../contexts/implementer/profile"
-import { useReducer } from "react"
-import { reducer } from "../../../contexts/implementer/profile/reducer"
+import { useState, useCallback, useMemo } from "react"
 
 export default function Profile() {
-  const [state, dispatch] = useReducer(reducer)
+  const [state, setState] = useState({ generalInformation: {} })
+
+  const setGeneralInformation = useCallback(() =>
+    generalInformation => {
+      return setState(prevState => {
+        return {
+          ...prevState,
+          generalInformation: {
+            ...prevState.generalInformation,
+            generalInformation
+          }
+        }
+      })
+    }
+  )
+
+  const injectActions = useMemo(() => { setGeneralInformation }, []) 
 
   return (
     <PageContext.Provider value={data}>
-      <ImplementerProfileContext.Provider value={{dispatch, state}}>
+      <ImplementerProfileContext.Provider value={injectActions()}>
         <Layout>
           <GeneralInformation />
         </Layout>
