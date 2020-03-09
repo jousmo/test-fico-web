fd0787d301e8d709ccd1178bf4e1dfded80024e7
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-09 13:26:52 -0600

    [#FZC-I84] #comment Set antd config

---
+++ antd.config.js
@@ -0,0 +1,10 @@
+import esES from "antd/es/locale/es_ES"
+
+export const antdConfig = {
+  form: {
+    validateMessages: {
+      required: "Es requerido llenar este campo"
+    }
+  },
+  locale: esES
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "antd/dist/antd.css"
+import { ConfigProvider } from "antd"
+import { antdConfig } from "../antd.config"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider {...antdConfig}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  )
 }
