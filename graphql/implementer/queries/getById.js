fddf09237e29fa976bfc6dcfed95fd9bc34bd06e
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-09 20:44:28 -0600

    [#FZC-I84] #comment Added implementer queries and mutations

--- graphql/implementer/queries/getById.js
+++ graphql/implementer/queries/getById.js
@@ -1,29 +1,27 @@
 import { gql } from "apollo-boost";
 
 export const getById = gql`
-  {
-    Query ImplementerById($id: String!) {
-      Implementer(id: $id) {
-        id
-        type
-        name
-        director
-        rfc
-        commercialName
-        commercialAddress
-        fiscalAddress
-        phone
-        proofOfCharitableContributions
-        legalRepresentative
-        email
-        mission
-        vision
-        history
-        institutionExperience
-        previousSupports
-        alliances
-        incomeAndExpenses
-      }
+  query ImplementerById($id: ID!) {
+    Implementer(id: $id) {
+      id
+      type
+      name
+      director
+      rfc
+      commercialName
+      commercialAddress
+      fiscalAddress
+      phone
+      proofOfCharitableContributions
+      legalRepresentative
+      email
+      mission
+      vision
+      history
+      institutionExperience
+      previousSupports
+      alliances
+      incomeAndExpenses
     }
   }
 `
