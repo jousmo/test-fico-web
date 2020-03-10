3761fc74603b30467bea56268f6fc61e2b5c7b93
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-10 12:24:52 -0600

    [#FZC-I84] #comment Fix typo in implementer types

--- helpers/selectOptions/implementer/profile/implementerTypes.js
+++ helpers/selectOptions/implementer/profile/implementerTypes.js
@@ -1,4 +1,4 @@
 export const implementerTypes = [
   { label: "Organizacion civil", value: "CIVIL_ORGANIZATION" },
-  { label: "GOBIERNO", value: "GOVERNMENT" }
+  { label: "Gobierno", value: "GOVERNMENT" }
 ]
