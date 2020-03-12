476873dae4b3bfe5387ba747c12650b7e3d12eca
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-12 17:05:32 -0600

    [#FZC-l88] #comment Implemented government form

--- components/implementer/profile/government/index.js
+++ components/implementer/profile/government/index.js
@@ -1,10 +1,22 @@
 import { Section } from "../../../shared";
 import { GovernmentForm } from "./form";
+import { ImplementerProfileContext } from "../../../../contexts/implementer/profile";
+import { useContext } from "react";
 
 export function Government() {
+  const {
+    updateGeneralInformation,
+    loading,
+    error,
+    data
+  } = useContext(ImplementerProfileContext)
+
   return (
     <Section title="5. Gobernanza">
-      <GovernmentForm />
+      <GovernmentForm
+        data={data}
+        isLoading={loading}
+        error={error} />
     </Section>
   )
 }
