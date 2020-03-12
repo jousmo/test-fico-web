11d1724082d5b684c89e0982385258033d6ed3ef
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-12 17:38:42 -0600

    [#FZC-l88] #comment Handle item updating in composite field

--- components/shared/composite-field/index.js
+++ components/shared/composite-field/index.js
@@ -11,15 +11,27 @@ export function CompositeField({
 }) {
   const [state, setState] = useState({ items: Array.from(defaultValue) })
   
-  const addNew = (value) => {
+  const addNew = value => {
     const newItems = [...state.items, value]
     setState({ items: newItems })
-    onChange && onChange(items)
+    onChange && onChange(newItems)
+  }
+
+  const updateItem = index => {
+    return event => {
+      const value = event.currentTarget?.value || event.target?.checked
+      const name = event.currentTarget?.id || event.target?.name
+      const newItems = Array.from(state.items)
+      newItems[index][name] = value
+
+      setState({ items: newItems })
+      onChange && onChange(newItems)
+    }
   }
 
   return (
     <div>
-      { children({items: state.items }) }
+      { children({items: state.items, updateItem: updateItem }) }
       <Button
         type="dashed"
         icon={<PlusOutlined />}
