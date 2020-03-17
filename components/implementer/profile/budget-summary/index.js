2473c01da40f9acd1b8dd8b05e917e963b15bbdc
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-17 12:18:59 -0600

    [#FZC-87] #comment Added budget summary component

---
+++ components/implementer/profile/budget-summary/index.js
@@ -0,0 +1,21 @@
+import { Section, SelectField } from "../../../shared";
+import { useContext } from "react";
+import { ImplementerProfileContext } from "../../../../contexts/implementer/profile";
+import { BudgetSummaryTable } from "./budgetSummaryTable";
+
+export function BudgetSummary() {
+  const {
+    loading,
+    error,
+    data
+  } = useContext(ImplementerProfileContext)
+
+  return (
+    <Section title="3. Resumen de presupuestos anuales">
+      <BudgetSummaryTable
+        data={data}
+        isLoading={loading}
+        error={error} />
+    </Section>
+  )
+}
