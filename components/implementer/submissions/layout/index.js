7a9f08a0503f7de57f46406ba2a60097a705677b
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-19 14:24:14 -0600

    [#FZC-97] #comment Move components from shared folder to submissions folder

--- components/implementer/submissions/layout/index.js
+++ components/implementer/submissions/layout/index.js
@@ -1,14 +1,13 @@
-import "./style.sass"
 import { Layout as ALayout } from "antd"
-import { MainMenu } from "../main-menu"
-import { Toolbar } from "../toolbar"
+import { Steps } from "../steps"
+import { Toolbar } from "../../../shared"
 
 export function Layout({children}) {
   return (
-    <div className="fico layout full-height">
+    <div className="fico layout submissions full-height">
       <ALayout className="full-height">
-        <ALayout.Header><Toolbar /></ALayout.Header>
         <ALayout.Content>
+          <Toolbar subheader={<Steps />} />
           <div className="full-height">
             {children}
           </div>
