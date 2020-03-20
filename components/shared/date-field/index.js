e97417afafa62f93589e4f725a0dbe41aec90e76
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-20 17:44:43 -0600

    [#FZC-97] #comment Added date field component

---
+++ components/shared/date-field/index.js
@@ -0,0 +1,30 @@
+import { DatePicker } from "antd"
+import moment from "moment"
+
+export function DateField({
+  id,
+  name,
+  onChange,
+  placeholder="Selecciona fecha...",
+  defaultValue,
+  fullWidth
+}) {
+  const onDateChange = value => {
+    onChange && onChange({
+      currentTarget: {
+        id: id,
+        value: value.format()
+      }
+    })
+  }
+
+  return (
+    <DatePicker
+      id={id}
+      name={name}
+      style={fullWidth && {width: "100%"}}
+      defaultValue={defaultValue && moment(defaultValue)}
+      placeholder={placeholder}
+      onChange={onDateChange} />
+  )
+}
