47d37adbffcebc803be1ac5b725c8c9807ab42ee
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-20 14:18:08 -0600

    [#FZC-97] #comment Pass mode option to select field

--- components/shared/selectField/index.js
+++ components/shared/selectField/index.js
@@ -6,6 +6,7 @@ export function SelectField({
   id,
   name,
   onChange,
+  mode,
   placeholder="Selecciona...",
   defaultValue
 }) {
@@ -24,6 +25,7 @@ export function SelectField({
       name={name}
       onChange={onSelectChange}
       placeholder={placeholder}
+      mode={mode}
       defaultValue={defaultValue}>
       { options.map((o, i) => 
         <Select.Option key={kebabCase(`${o.value}-${i}`)} value={o.value}>{o.label}</Select.Option>
