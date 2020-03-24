4fe78bf22d68fb47fbd30ab936a4f49b4b99360e
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-23 18:59:42 -0600

    [#FZC-I102] #comment Fix row indent

--- components/implementer/submissions/new/general-information/development-objectives/form/index.js
+++ components/implementer/submissions/new/general-information/development-objectives/form/index.js
@@ -18,65 +18,65 @@ function DevelopmentObjectivesForm({data, onChange}) {
       name="project-details"
       layout="vertical">
       <Row gutter={[10, 8]} justify="start">
-      <Col span={24}>
-        <Form.Item
-          style={{display: "inline"}}
-          label="Objetivo de desarrollo">
-          <Input.TextArea
-            id="developmentObjective"
-            name="developmentObjective"
-            defaultValue={data?.Submission?.developmentObjective}
-            onChange={onChange}
-            autoSize={{minRows: 3}} />
-        </Form.Item>
-      </Col>
-      <Col span={24}>
-        <Form.Item
-          style={{display: "inline"}}
-          label="Objetivo de general">
-          <Input.TextArea
-            id="generalObjective"
-            name="generalObjective"
-            defaultValue={data?.Submission?.generalObjective}
-            onChange={onChange}
-            autoSize={{minRows: 3}} />
-        </Form.Item>
-      </Col>
-      <Col span={24}>
-        <Form.Item
-          style={{display: "inline"}}
-          label="Objetivos específicos (máximo 5)">
-          <CompositeField
-            maxItems={5}
-            onChange={onSpecificObjectivesChange}
-            defaultValue={data?.Submission?.specificObjectives || []}
-            addLabel="Agregar objetivo específico"
-            onClickAdd={(addNew) => addNew({description: ""})}>
-            {({ items, updateItem, removeItem }) => 
-              <div>
-                { items.map((item, index) => 
-                  <Form.Item key={`specific_objective_${index}`}>
-                    <Row>
-                      <Col flex="auto">
-                        <Input.TextArea
-                          id="description"
-                          name="description"
-                          defaultValue={item.description}
-                          onChange={updateItem(index)}
-                          autoSize={{minRows: 3}} />
-                      </Col>
-                      <Col>
-                        <DeleteButton style={{marginLeft: "10px"}} onClick={() => removeItem(index)} />
-                      </Col>
-                    </Row>
-                  </Form.Item>
-                ) }
-              </div>
-            }
-          </CompositeField>
-        </Form.Item>
-      </Col>
-    </Row>
+        <Col span={24}>
+          <Form.Item
+            style={{display: "inline"}}
+            label="Objetivo de desarrollo">
+            <Input.TextArea
+              id="developmentObjective"
+              name="developmentObjective"
+              defaultValue={data?.Submission?.developmentObjective}
+              onChange={onChange}
+              autoSize={{minRows: 3}} />
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Form.Item
+            style={{display: "inline"}}
+            label="Objetivo de general">
+            <Input.TextArea
+              id="generalObjective"
+              name="generalObjective"
+              defaultValue={data?.Submission?.generalObjective}
+              onChange={onChange}
+              autoSize={{minRows: 3}} />
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Form.Item
+            style={{display: "inline"}}
+            label="Objetivos específicos (máximo 5)">
+            <CompositeField
+              maxItems={5}
+              onChange={onSpecificObjectivesChange}
+              defaultValue={data?.Submission?.specificObjectives || []}
+              addLabel="Agregar objetivo específico"
+              onClickAdd={(addNew) => addNew({description: ""})}>
+              {({ items, updateItem, removeItem }) => 
+                <div>
+                  { items.map((item, index) => 
+                    <Form.Item key={`specific_objective_${index}`}>
+                      <Row>
+                        <Col flex="auto">
+                          <Input.TextArea
+                            id="description"
+                            name="description"
+                            defaultValue={item.description}
+                            onChange={updateItem(index)}
+                            autoSize={{minRows: 3}} />
+                        </Col>
+                        <Col>
+                          <DeleteButton style={{marginLeft: "10px"}} onClick={() => removeItem(index)} />
+                        </Col>
+                      </Row>
+                    </Form.Item>
+                  ) }
+                </div>
+              }
+            </CompositeField>
+          </Form.Item>
+        </Col>
+      </Row>
     </Form>
   )
 }
