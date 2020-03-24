30751220c65fa7a1b435952d76a67b51205e3dee
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-24 17:32:09 -0600

    [#FZC-I101] #comment Pass style to upload button

--- components/shared/upload-button/index.js
+++ components/shared/upload-button/index.js
@@ -1,10 +1,10 @@
 import { Upload, Button } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 
-export function UploadButton({children}) {
+export function UploadButton({children, style}) {
   return (
     <Upload>
-      <Button>
+      <Button style={style}>
         <UploadOutlined /> {children}
       </Button>
     </Upload>
