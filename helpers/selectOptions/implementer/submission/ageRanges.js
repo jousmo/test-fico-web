642ac53ef1f9694210aee1dab200cda316974f4b
Rodrigo D. Cuervo <rod750@gmail.com>
2020-03-26 14:44:02 -0600

    [#FZC-I103] #comment Added age ranges types

---
+++ helpers/selectOptions/implementer/submission/ageRanges.js
@@ -0,0 +1,8 @@
+export const ageRanges = [
+  { label: "0 a 5 años", value: "0-5" },
+  { label: "6 a 11 años", value: "6-11" },
+  { label: "12 a 17 años", value: "12-17" },
+  { label: "18 a 29 años", value: "18-29" },
+  { label: "30 a 59 años", value: "30-59" },
+  { label: "60 años y más", value: "60+" }
+]
