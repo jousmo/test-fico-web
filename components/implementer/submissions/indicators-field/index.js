cfa26f24b0195503231549b339e48c9bc5e0551a
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-02 16:31:54 -0600

    [#FZC-I113] #comment Implemented composite field in indicators field component

--- components/implementer/submissions/indicators-field/index.js
+++ components/implementer/submissions/indicators-field/index.js
@@ -1,6 +1,29 @@
+import { IndicatorItem } from "./indicator-item"
+import { CompositeField } from "../../../shared"
+import { useState } from "react"
 
-export function IndicatorsField({defaultValue, onChange, key}) {
+export function IndicatorsField({defaultValue, onChange}) {
+  const [state, setState] = useState({ isModalOpen: false })
+
+  const onClickAdd = () => {
+    setState({ isModalOpen: true })
+  }
+
   return (
-    <></>
+    <CompositeField
+      onChange={onChange}
+      defaultValue={defaultValue}
+      onClickAdd={onClickAdd}
+      addLabel="Agregar indicador">
+      {({ items, addNew, removeItem }) =>
+        <div>
+          { items.map((item, index) =>
+            <IndicatorItem
+              data={item}
+              key={`indicator_${index}`} />
+          ) }
+        </div>
+      }
+    </CompositeField>
   )
 }
