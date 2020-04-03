4e0aa4e33572d0064271191e654773c815feb596
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-03 15:53:39 -0600

    [#FZC-I113] #comment Implement add item in multiple text field

--- components/shared/multiple-text-field/index.js
+++ components/shared/multiple-text-field/index.js
@@ -1,5 +1,7 @@
 import { Form, Input } from "antd"
 import { CompositeField } from "../composite-field"
+import { DeleteButton } from "../delete-button"
+import { v4 as uuid } from "uuid"
 
 export function MultipleTextField({
   onChange,
@@ -10,14 +12,16 @@ export function MultipleTextField({
     <CompositeField
       onChange={onChange}
       addLabel={addLabel}
-      defaultValue={defaultValue}>
-      {(items, removeItem, updateItem) =>
+      defaultValue={defaultValue}
+      onClickAdd={addNew => addNew({ value: "", uuid: uuid() })}>
+      {({items, removeItem, updateItem}) =>
         <div>
           {items.map((item, index) =>
             <Form.Item key={item.uuid}>
               <Input
                 onChange={updateItem(index)}
                 defaultValue={item.value} />
+              <DeleteButton onClick={removeItem(index)} />
             </Form.Item>
           )}
         </div>
