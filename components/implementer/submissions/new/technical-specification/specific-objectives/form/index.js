fd7a3590ba4e76d794c3bf948b6a7fad5847fbaa
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-15 12:31:20 -0500

    [#FZC-121] #comment Implement indicators field in specific objective

--- components/implementer/submissions/new/technical-specification/specific-objectives/form/index.js
+++ components/implementer/submissions/new/technical-specification/specific-objectives/form/index.js
@@ -1,6 +1,7 @@
 import { Section } from "../../../../../../shared"
 import { v4 as uuid } from "uuid"
-import { Skeleton, Alert } from "antd"
+import { Skeleton, Alert, Form } from "antd"
+import { IndicatorsField } from "../../../../indicators-field"
 
 export default function SpecificObjectiveForm({
   data,
@@ -26,12 +27,31 @@ export default function SpecificObjectiveForm({
   }
 
   const specificObjectives = data?.Submission?.specificObjectives || []
+  
+  const onSpecificObjectiveIndicatorsChange = (index) => (indicators) => {
+    const newSpecificObjectives = Array.from(specificObjectives)
+
+    newSpecificObjectives[index].indicators = indicators
+
+    onChange && onChange(newSpecificObjectives)
+  }
 
   return (
     <>
       { specificObjectives.map((objective, index) =>
         <Section title={`Objetivo ${index + 1}`} key={uuid()}>
-          {objective.description}
+          <Form
+            name="specific-objective-indicators"
+            layout="vertical">
+            <Form.Item>
+              {objective.description}
+            </Form.Item>
+            <Form.Item label="Indicadores">
+            <IndicatorsField
+              onChange={onSpecificObjectiveIndicatorsChange(index)}
+              defaultValue={objective.indicators} />
+            </Form.Item>
+          </Form>
         </Section>
       ) }
     </>
