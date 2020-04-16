de21b0ad22645bd5c460d50db5d3d236c1e3249b
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-16 14:02:09 -0500

    [#FZC-I124] #comment Added support for date ranges to date field

--- components/shared/date-field/index.js
+++ components/shared/date-field/index.js
@@ -8,15 +8,39 @@ export function DateField({
   placeholder="Selecciona fecha...",
   defaultValue,
   fullWidth,
-  value
+  value,
+  range,
+  ...props
 }) {
   const onDateChange = value => {
+    if(value?.length > 0) {
+      value = value.map(v => v.format("YYYYMM[01]"))
+      console.log(value)
+    }
+    else {
+      value = value?.format()
+    }
+
     onChange && onChange({
       currentTarget: {
         id: id,
-        value: value?.format()
+        value: value
       }
     })
+  }
+
+  if(range) {
+    return (
+      <DatePicker.RangePicker
+        id={id}
+        name={name}
+        style={fullWidth && {width: "100%"}}
+        defaultValue={defaultValue && defaultValue.map(v => moment(v))}
+        placeholder={placeholder}
+        onChange={onDateChange}
+        value={value && value.map(v => moment(v))}
+        {...props} />
+    )
   }
 
   return (
@@ -27,6 +51,7 @@ export function DateField({
       defaultValue={defaultValue && moment(defaultValue)}
       placeholder={placeholder}
       onChange={onDateChange}
-      value={value && moment(value)} />
+      value={value && moment(value)}
+      {...props} />
   )
 }
