69842c92e237109196ebe6191308581868bbdfea
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-16 10:49:08 -0500

    [#FZC-I124] #comment Added activities field component

---
+++ components/implementer/submissions/activities-field/index.js
@@ -0,0 +1,59 @@
+import { IndicatorItem } from "./indicator-item"
+import { CompositeField } from "../../../shared"
+import { useState } from "react"
+import { IndicatorModal } from "./indicator-modal"
+
+export function ActivitiesField({defaultValue, onChange}) {
+  const [state, setState] = useState({ isModalOpen: false, edit: undefined })
+
+  const onClickAdd = () => {
+    setState({ isModalOpen: true })
+  }
+
+  const onCancel = () => {
+    setState({ isModalOpen: false, edit: undefined })
+  }
+
+  const onSave = (addNew, replaceItemAtIndex) => (values) => {
+    if(typeof values.index === "undefined") {
+      addNew(values)
+    }
+    else {
+      const index = values.index
+      delete values.index
+
+      replaceItemAtIndex(index, values)
+    }
+    onCancel()
+  }
+
+  const onEdit = (data, index) => () => {
+    data.index = index
+    setState({ isModalOpen: true, edit: data })
+  }
+
+  return (
+    <CompositeField
+      onChange={onChange}
+      defaultValue={defaultValue}
+      onClickAdd={onClickAdd}
+      addLabel="Agregar actividad">
+      {({ items, addNew, removeItem, replaceItemAtIndex }) =>
+        <div>
+          <ActivityModal
+            onCancel={onCancel}
+            onSave={onSave(addNew, replaceItemAtIndex)}
+            visible={state.isModalOpen}
+            edit={state.edit} />
+          { items.map((item, index) =>
+            <ActivityItem
+              data={item}
+              key={`indicator_${item.uuid}`}
+              onDelete={removeItem(index)}
+              onEdit={onEdit(item, index)} />
+          ) }
+        </div>
+      }
+    </CompositeField>
+  )
+}
