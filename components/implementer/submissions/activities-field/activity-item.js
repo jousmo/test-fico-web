ca2b3e7f6259fc6d9a57901c9393faf7101e5817
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-16 14:03:05 -0500

    [#FZC-I124] #comment Display activity data

--- components/implementer/submissions/activities-field/activity-item.js
+++ components/implementer/submissions/activities-field/activity-item.js
@@ -3,35 +3,21 @@ import { EditButton, DeleteButton } from "../../../shared"
 
 export function ActivityItem({data, onDelete, onEdit}) {
   const {
-    title="Indicador sin título",
     description="N/A",
-    methodology="N/A",
+    responsible="N/A",
     baseline="N/A",
     goal="N/A",
-    formula="N/A",
-    inputs,
     meansOfVerification="N/A",
+    place="N/A",
+    months="N/A",
     key
   } = data
 
-  const formatInputs = inputs => {
-    if(!inputs?.length) {
-      return "N/A"
-    }
-
-    return `${inputs.join(", ")}.`
-  }
-
   return (
     <Card key={`indicator_${key}`} style={{marginBottom: "20px"}}>
-      <Typography.Title level={4}>{title}</Typography.Title>
+      <Typography.Title level={4}>{description}</Typography.Title>
       <Typography.Text type="secondary">
-        {description}
-      </Typography.Text>
-      <br />
-      <Typography.Text strong>Metodología: </Typography.Text>
-      <Typography.Text>
-        {methodology}
+        {responsible}
       </Typography.Text>
       <br />
       <Typography.Text strong>Línea base: </Typography.Text>
@@ -40,14 +26,14 @@ export function ActivityItem({data, onDelete, onEdit}) {
       <Typography.Text strong>Meta: </Typography.Text>
       <Typography.Text>{goal}</Typography.Text>
       &nbsp;
-      <Typography.Text strong>Formula: </Typography.Text>
-      <Typography.Text>{formula}</Typography.Text>
-      &nbsp;
-      <Typography.Text strong>Insumos: </Typography.Text>
-      <Typography.Text>{formatInputs(inputs)}</Typography.Text>
-      &nbsp;
       <Typography.Text strong>Medio de verificación: </Typography.Text>
       <Typography.Text>{meansOfVerification}</Typography.Text>
+      <br />
+      <Typography.Text strong>Lugar de intervención: </Typography.Text>
+      <Typography.Text>{place}</Typography.Text>
+      &nbsp;
+      <Typography.Text strong>Mes de implementación: </Typography.Text>
+      <Typography.Text>{months}</Typography.Text>
       <DeleteButton
         onClick={onDelete}
         style={{marginLeft: "8px"}} />
