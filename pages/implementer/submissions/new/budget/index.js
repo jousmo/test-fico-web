62c61072fc57a517cfa2d351107948959124e64b
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-21 12:21:33 -0500

    [#FZC-I156] #comment Added budget page component

---
+++ pages/implementer/submissions/new/budget/index.js
@@ -0,0 +1,76 @@
+import { Layout } from "../../../../../components/implementer/submissions"
+import {
+  data as pageData,
+  ImplementerSubmissionContext
+} from "../../../../../contexts/implementer/submissions/new"
+import { PageContext } from "../../../../../contexts/page"
+import { submission } from "../../../../../graphql/submission"
+import { useState, useCallback, useMemo } from "react"
+import { useMutation, useQuery } from "@apollo/react-hooks"
+import { withApollo } from "../../../../../helpers/withApollo"
+import {
+  Budget as BudgetTable
+} from "../../../../../components/implementer/submissions/new/technical-specification"
+
+
+function Budget({ client }) {
+  const [state, setState] = useState({
+    budget: {},
+    dirty: false
+  })
+
+  const [updateSubmission] = useMutation(
+    submission.mutations.updateById, { client: client }
+  )
+
+  const { loading, error, data } = useQuery(submission.queries.getById, {
+    client: client,
+    variables: { id: "1" }
+  })
+
+  const updateBudget = useCallback(budget => {
+    const newBudget = {
+      ...state.budget,
+      ...budget
+    }
+
+    setState({
+      ...state,
+      dirty: true,
+      budget: budget
+    })
+  })
+
+  const save = useCallback(async () => {
+    try {
+      const updatedSubmission = await updateSubmission({
+        variables: { ...state.budget, id: "1" }
+      })
+
+      /* TODO: Show feedback to the user */
+    }
+    catch(e) {
+      console.error(e)
+    }
+  })
+
+  const injectActions = useMemo(() => ({
+    updateBudget,
+    save,
+    loading,
+    error,
+    data
+  }), [state, loading])
+
+  return (
+    <PageContext.Provider value={pageData({ save, step: 3 })}>
+      <ImplementerSubmissionContext.Provider value={injectActions}>
+        <Layout>
+          <BudgetTable />
+        </Layout>
+      </ImplementerSubmissionContext.Provider>
+    </PageContext.Provider>
+  )
+}
+
+export default withApollo(Budget)
