45d4b8370932cd7c316e8d5eedfee1dc05208415
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-04-21 11:31:20 -0600

    [#FZC-I151] Added ActivityTooltip component

---
+++ components/implementer/submissions/new/schedule/activities-schedule/table/activityTooltip.js
@@ -0,0 +1,18 @@
+import { Typography } from 'antd';
+
+function ActivityTooltip({ activity }) {
+  return (
+    <div>
+      <Typography.Text strong>Actividad</Typography.Text><br/>
+      <Typography.Text type="secondary">{activity.description}</Typography.Text><br/>
+      <Typography.Text strong>Meta</Typography.Text><br/>
+      <Typography.Text type="secondary">{activity.goal} Asistentes</Typography.Text><br/>
+      <Typography.Text strong>Medida de Verificación</Typography.Text><br/>
+      <Typography.Text type="secondary">{activity.meansOfVerification}</Typography.Text><br/>
+      <Typography.Text strong>Presupuesto designado</Typography.Text><br/>
+      <Typography.Text type="secondary">${activity.amount}</Typography.Text><br/>
+    </div>
+  )
+}
+
+export default ActivityTooltip
