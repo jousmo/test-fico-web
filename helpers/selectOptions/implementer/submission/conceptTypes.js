5e802dd9655fd263c4595f243db2b7594a1e2966
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-22 16:19:14 -0500

    [#FZC-I156] #comment Added concept types

---
+++ helpers/selectOptions/implementer/submission/conceptTypes.js
@@ -0,0 +1,11 @@
+export const conceptTypes = [
+  { label: "Recurso humano", value: "HUMAN_RESOURCE" },
+  { label: "Materiales y suministros", value: "SUPPLIES" },
+  { label: "Equipamiento", value: "EQUIPMENT" },
+  { label: "Gastos de oficina", value: "OFFICE_EXPENSES" },
+  { label: "Seguimiento y evaluación", value: "MONITORING" },
+  { label: "Promoción y publicidad", value: "MARKETING" },
+  { label: "Eventos", value: "EVENTS" },
+  { label: "Capacitación", value: "TRAINING" },
+  { label: "Gastos financieros", value: "FINANCIAL_EXPENSES" }
+]
