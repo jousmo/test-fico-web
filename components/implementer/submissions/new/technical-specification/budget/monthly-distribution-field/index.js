59915efaabdc63320795828e64441873aa8bdabf
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-22 17:44:08 -0500

    [#FZC-I156] #comment Implemented data load in monthly distribution component

--- components/implementer/submissions/new/technical-specification/budget/monthly-distribution-field/index.js
+++ components/implementer/submissions/new/technical-specification/budget/monthly-distribution-field/index.js
@@ -1,5 +1,53 @@
-export function MonthlyDistributionField() {
+import * as _ from "lodash"
+import { Input, Row, Col } from "antd"
+import { createRef } from "react"
+
+export function MonthlyDistributionField({
+  defaultValue=[],
+  months=[],
+  unitCost=1.0
+}) {
+  const rows = months.map((label, index) => {
+    const data = {}
+
+    data.label = label
+    data.value = defaultValue[index]
+    data.inputRef = createRef()
+
+    return data
+  })
+
+  const total = rows.reduce((t, r) => t + r.value, 0)
+
   return (
-    
+    <>
+      <Row>
+        <Col span={12}>Mes</Col>
+        <Col span={6}>Unidades</Col>
+        <Col span={6}>Costo</Col>
+      </Row>
+
+      {rows.map((r, i) =>
+        <Row key={`month-${i}`}>
+          <Col span={12}>{ r.label }</Col>
+          <Col span={6}>
+            <Input
+              type="text"
+              name={`month-${i}`}
+              defaultValue={r.value}
+              ref={r.inputRef} />
+          </Col>
+          <Col span={6}>
+            { unitCost * Number(r.inputRef.value()) }
+          </Col>
+        </Row>
+      )}
+
+      <Row>
+        <Col span={12}>Total</Col>
+        <Col span={6}></Col>
+        <Col span={6}>{ total }</Col>
+      </Row>
+    </>
   )
 }
