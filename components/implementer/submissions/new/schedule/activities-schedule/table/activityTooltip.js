18edc0c97ed82b23c234575e3e87f6e242f35a92
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-04-21 20:10:56 -0600

    [#FZC-I151] Finished ActivitiesTable component using real data from context and MomentJs for dates

--- components/implementer/submissions/new/schedule/activities-schedule/table/activityTooltip.js
+++ components/implementer/submissions/new/schedule/activities-schedule/table/activityTooltip.js
@@ -1,16 +1,23 @@
 import { Typography } from 'antd';
 
-function ActivityTooltip({ activity }) {
+function ActivityTooltip({ activity = {} }) {
+  const {
+    description = "N/A",
+    goal = "N/A",
+    meansOfVerification = "N/A",
+    baseline = "N/A"
+  } = activity
+
   return (
     <div>
       <Typography.Text strong>Actividad</Typography.Text><br/>
-      <Typography.Text type="secondary">{activity.description}</Typography.Text><br/>
+      <Typography.Text type="secondary">{description}</Typography.Text><br/>
       <Typography.Text strong>Meta</Typography.Text><br/>
-      <Typography.Text type="secondary">{activity.goal} Asistentes</Typography.Text><br/>
+      <Typography.Text type="secondary">{goal}</Typography.Text><br/>
       <Typography.Text strong>Medida de Verificación</Typography.Text><br/>
-      <Typography.Text type="secondary">{activity.meansOfVerification}</Typography.Text><br/>
+      <Typography.Text type="secondary">{meansOfVerification}</Typography.Text><br/>
       <Typography.Text strong>Presupuesto designado</Typography.Text><br/>
-      <Typography.Text type="secondary">${activity.baseline}</Typography.Text><br/>
+      <Typography.Text type="secondary">${baseline}</Typography.Text><br/>
     </div>
   )
 }
--- components/implementer/submissions/new/schedule/activities-schedule/table/index.js
+++ components/implementer/submissions/new/schedule/activities-schedule/table/index.js
@@ -1,6 +1,9 @@
 import { withForm } from "../../../../../../../helpers/withForm"
-import { Table } from "antd"
 import { FieldLabel } from "../../../../../../shared/field-label";
+import { Table } from "antd"
+import Moment from "moment"
+import { extendMoment } from "moment-range"
+const moment = extendMoment(Moment)
 import ActivityTooltip from "./activityTooltip"
 import ActivityBox from "./activityBox"
 
@@ -8,7 +11,7 @@ function ActivitiesTable({ data }) {
   const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
   const currentYear = new Date().getFullYear()
 
-  const activities = data?.Submission?.specificObjectives.map(objective => objective.activities.flat()).flat() || []
+  const activities = data?.Submission?.specificObjectives?.map(({ activities }) => activities).flat()
 
   const getLabel = (record, index) => {
     const activity = activities[index]
@@ -18,31 +21,33 @@ function ActivitiesTable({ data }) {
     )
   }
 
-  const columns = [
-    {
-      title: 'Actividad',
-      dataIndex: 'activity',
-      key: 'activity',
-      render: (text, record, index) => getLabel(record, index)
-    },
-  ].concat(months.map(month => ({
-      title: `${month} ${currentYear}`,
-      dataIndex: month,
-      key: month,
-  })))
-
-  const dataSource = activities.map((activity, index) => {
+  const dataSource = activities[0] && activities.map((activity, index) => {
     let result = {
       key: index,
       activity: `Actividad ${index + 1}`
     }
-    activity.months.forEach(month => (
-      result[month.substring(0,3)] = <ActivityBox />
+    const range = moment.range(activity.months[0], activity.months[1]);
+    const months = Array.from(range.by('month')).map(m => m.format('YYYYM'))
+    months.forEach(month => (
+      result[month] = <ActivityBox />
     ))
     return result
   })
 
-  return <Table dataSource={dataSource} columns={columns} pagination={false} scroll={{ x: true }} />;
+  return (
+    <Table dataSource={dataSource} pagination={false} scroll={{ x: true }}>
+      <Table.Column
+        dataIndex="activity"
+        render={(text, record, index) => getLabel(record, index)}
+        title="Actividad" />
+      {months.map((month, index) => (
+        <Table.Column
+          key={index}
+          dataIndex={`${currentYear}${index + 1}`}
+          title={`${month} ${currentYear}`} />
+      ))}
+    </Table>
+  )
 }
 
 export default withForm(ActivitiesTable)
