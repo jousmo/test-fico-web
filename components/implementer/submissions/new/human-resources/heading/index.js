e85b2fb8a9e1eb1f4ed38d00656a6f271101c47d
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-04-22 17:08:34 -0600

    [#FZC-I155] Added Heading component

---
+++ components/implementer/submissions/new/human-resources/heading/index.js
@@ -0,0 +1,15 @@
+import { Section } from "../../../../../shared"
+import { Typography } from "antd"
+
+export function Heading() {
+  return (
+    <Section>
+      <Typography.Text>Recursos Humanos</Typography.Text>
+      <br />
+      <Typography.Text type="secondary">
+        Registra a cada miembro que será parte de la implementación de este
+        proyecto, ten en cuanta que ya debiste registrar.
+      </Typography.Text>
+    </Section>
+  )
+}
