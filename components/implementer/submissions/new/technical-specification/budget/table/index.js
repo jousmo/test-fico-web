eb01c0b3851c0c687339f5fa1c377d10975f8cf7
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-23 16:17:11 -0500

    [#FZC-I156] #comment Implement budget table data display

--- components/implementer/submissions/new/technical-specification/budget/table/index.js
+++ components/implementer/submissions/new/technical-specification/budget/table/index.js
@@ -1,7 +1,45 @@
 import { withForm } from "../../../../../../../helpers"
+import { CompositeField } from "../../../../../../shared"
+import { Empty, Row, Col } from "antd"
+import { useState } from "react"
+import { ConceptModal } from "./concept-modal"
 
 function BudgetTable({data, onChange}) {
-  
+  const [state, setState] = useState({ isModalOpen: false })
+
+  const onClickAdd = () => {
+    setState({ ...state, isModalOpen: true })
+  }
+
+  const onConceptsChange = (concepts) => {
+    console.log(concepts)
+  }
+
+  return (
+    <>
+      <ConceptModal visible={state.isModalOpen} />
+      <CompositeField
+        onClickAdd={onClickAdd}
+        onChange={onConceptsChange}
+        addLabel="Agregar concepto">
+        {({ items }) =>
+          <>
+            {items.map(i =>
+              <Row>
+                <Col>{JSON.stringify(i)}</Col>
+              </Row>
+            )}
+            {!items.length ?
+              <Empty
+                style={{marginBottom: "10px"}}
+                description="Agrega todos los conceptos que
+                serán utilizados durante el proyecto" />
+            : null }
+          </>
+        }
+      </CompositeField>
+    </>
+  )
 }
 
 export default withForm(BudgetTable)
