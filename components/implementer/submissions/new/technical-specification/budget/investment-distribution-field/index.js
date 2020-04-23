5d85ad978a9310e4469f8ee755d9e2e72d133542
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-23 11:30:59 -0500

    [#FZC-I156] #comment Implement data load in investment distribution field

--- components/implementer/submissions/new/technical-specification/budget/investment-distribution-field/index.js
+++ components/implementer/submissions/new/technical-specification/budget/investment-distribution-field/index.js
@@ -1,5 +1,50 @@
-export default InvestmentDistributionField() {
+import { Row, Col, Input } from "antd"
+
+export function InvestmentDistributionField({
+  defaultValue = [
+    { type: "IMPLEMENTER", label: "Implementadora", percentage: undefined },
+    { type: "FICOSEC", label: "FICOSEC", percentage: undefined },
+    { type: "ALLIED", label: "Aliado (s)", percentage: undefined }
+  ],
+  unitCost = 1.0,
+  unitTotal = 1,
+  onChange
+}) {
+  const rows = defaultValue.map(r => {
+    r.inputRef = createRef()
+
+    return r
+  })
+
+  const calculatePercentage = (percentage) => {
+    const total = unitCost * unitTotal
+
+    return percentage * total / 100
+  }
+
   return (
-    
+    <>
+      <Row>
+        <Col span={12}>Inversor</Col>
+        <Col span={6}>Porcentage</Col>
+        <Col span={6}>Total</Col>
+      </Row>
+
+      {rows.map((r, i) =>
+        <Row key={`investment-${i}`}>
+          <Col span={12}>{ r.label }</Col>
+          <Col span={6}>
+            <Input
+              type="text"
+              name={`investment-${i}`}
+              defaultValue={r.value}
+              ref={r.inputRef} />
+          </Col>
+          <Col span={6}>
+            { calculatePercentage(Number(r.inputRef.value())) }
+          </Col>
+        </Row>
+      )}
+    </>
   )
 }
