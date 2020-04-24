e0b35d0b6d8720f54ad0aae478a792f2eab74343
Rodrigo D. Cuervo <rod750@gmail.com>
2020-04-24 14:38:33 -0500

    [#FZC-I156] #comment Refactor monthly distribution field

--- components/implementer/submissions/new/technical-specification/budget/fields/monthly-distribution-field/index.js
+++ components/implementer/submissions/new/technical-specification/budget/fields/monthly-distribution-field/index.js
@@ -1,11 +1,12 @@
 import * as _ from "lodash"
 import { Input, Row, Col } from "antd"
-import { createRef } from "react"
+import { CompositeField } from "../../../../../../../shared"
+import numeral from "numeral"
 
 export function MonthlyDistributionField({
   defaultValue = [],
   months = [],
-  unitCost = 1.0,
+  unitCost = 0.0,
   onChange
 }) {
   const rows = months.map((label, index) => {
@@ -13,42 +14,54 @@ export function MonthlyDistributionField({
 
     data.label = label
     data.value = defaultValue[index]
-    data.inputRef = createRef()
 
     return data
   })
 
-  const total = rows.reduce((t, r) => t + r.value, 0)
+  const displayMonthTotal = (value) =>
+    numeral(unitCost * Number(value || 0)).format("$0,0.00") 
 
   return (
-    <>
-      <Row>
-        <Col span={12}>Mes</Col>
-        <Col span={6}>Unidades</Col>
-        <Col span={6}>Costo</Col>
-      </Row>
+    <CompositeField
+      defaultValue={rows}
+      isAddDisabled
+      onChange={onChange}>
+      {({ items, updateItem }) => {
+        const total = items.reduce((t, r) => t + unitCost * Number(r.value || 0), 0)
 
-      {rows.map((r, i) =>
-        <Row key={`month-${i}`}>
-          <Col span={12}>{ r.label }</Col>
-          <Col span={6}>
-            <Input
-              type="text"
-              name={`month-${i}`}
-              defaultValue={r.value}
-              ref={r.inputRef} />
-          </Col>
-          <Col span={6}>
-            { unitCost * Number(r.inputRef.current?.value || 0) }
-          </Col>
-        </Row>
-      )}
+        return <>
+          <Row gutter={[10, 8]}>
+            <Col span={12}>Mes</Col>
+            <Col span={6}>Unidades</Col>
+            <Col span={6}>Costo</Col>
+          </Row>
 
-      <Row>
-        <Col span={12}>Total</Col>
-        <Col span={6}></Col>
-        <Col span={6}>{ total }</Col>
-      </Row>
-    </>
+          { items.map((item, index) =>
+            <Row
+              gutter={[10, 8]}
+              key={`monthly-distribution-${item.uuid}`}>
+              <Col span={12}>{ item.label }</Col>
+              <Col span={6}>
+                <Input
+                  type="text"
+                  name="value"
+                  defaultValue={item.value}
+                  onChange={updateItem(index)} />
+              </Col>
+              <Col span={6}>
+                { displayMonthTotal(item.value) }
+              </Col>
+            </Row>
+          )}
+
+          <Row gutter={[10, 8]}>
+            <Col span={12}>Total</Col>
+            <Col span={6}></Col>
+            <Col span={6}>{ numeral(total).format("$0,0.00") }</Col>
+          </Row>
+        </>
+      }
+      }
+    </CompositeField>
   )
 }
