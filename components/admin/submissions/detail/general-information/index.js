974e5c30484de266cd35c6c45a0b01a9df83d277
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-04-27 15:26:20 -0600

    [#FZC-I163] Added GeneralInformation component

---
+++ components/admin/submissions/detail/general-information/index.js
@@ -0,0 +1,23 @@
+import { Section } from "../../../../../components/shared"
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../../contexts/admin/submissions/detail"
+import SubmissionObjectives from "./objectives"
+
+export function GeneralInformation() {
+  const {
+    loading,
+    error,
+    data
+  } = useContext(AdminSubmissionContext)
+
+  return (
+    <Section title="Información general">
+      <SubmissionObjectives
+        data={data}
+        error={error}
+        isLoading={loading} />
+    </Section>
+  )
+}
