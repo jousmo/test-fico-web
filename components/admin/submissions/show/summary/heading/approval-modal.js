7e98bb2c51a355ac745b50964549c8e96844aa40
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-04-29 12:51:18 -0600

    [#FZC-I165] Added ApprovalModal component

---
+++ components/admin/submissions/show/summary/heading/approval-modal.js
@@ -0,0 +1,61 @@
+import { useForm } from "antd/lib/form/util"
+import { Modal, Form, Row, Col, Input, Typography } from "antd"
+
+export function ApprovalModal({ onSave, onCancel, ...props }) {
+  const [form] = useForm()
+
+  const onOk = async () => {
+    try {
+      const values = await form.getFieldsValue()
+
+      onSave(values)
+      form.resetFields()
+    }
+    catch(e) {
+      console.error(e)
+    }
+  }
+
+  const onCancelModal = () => {
+    form.resetFields()
+    onCancel && onCancel()
+  }
+
+  return (
+    <Modal
+      title="Aprobación para consejo directivo"
+      onOk={onOk}
+      onCancel={onCancelModal}
+      width={800}
+      okText="Enviar"
+      cancelText="Cancelar"
+      {...props}>
+      <Form
+        form={form}
+        name="indicator-form"
+        layout="vertical">
+        <Row justify="start">
+          <Col flex="auto">
+            <Typography.Text>
+              Opinión técnica
+            </Typography.Text>
+            <br />
+            <Typography.Text type="secondary" style={{fontSize: "10.5px"}}>
+              En caso de que el consejo directivo necesite tomar en cuenta
+              algunos puntos especiales para la aprobación de la solicitud
+              describelo a continuación
+            </Typography.Text>
+            <Form.Item
+              name="technical_opinion"
+              style={{display: "inline"}}>
+              <Input.TextArea
+                id="technical_opinion"
+                type="text"
+                placeholder="Describe" />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  )
+}
