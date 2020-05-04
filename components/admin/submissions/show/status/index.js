ddc791c66b18b10a9eb2d570b74ef778f6578275
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-04 11:13:41 -0600

    [#FZC-I164] Added logic to update limit date on Status component

--- components/admin/submissions/show/status/index.js
+++ components/admin/submissions/show/status/index.js
@@ -1,23 +1,36 @@
 import { Section } from "../../../../shared"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import {
   AdminSubmissionContext
 } from "../../../../../contexts/admin/submissions/show"
 import StatusBody from "./body"
 
 export function Status() {
+  const [ isSaveHidden, setIsSaveHidden] = useState(true)
   const {
+    updateSubmissionDetail,
+    save,
     loading,
     error,
     data
   } = useContext(AdminSubmissionContext)
+
+  const onChange = newLimitDate => {
+    setIsSaveHidden(false)
+    const newSubmission = {...data?.Submission}
+    newSubmission.status.limit = newLimitDate
+    updateSubmissionDetail(newSubmission)
+  }
 
   return (
     <Section title="Estatus de solicitud">
       <StatusBody
         data={data?.Submission}
         error={error}
-        isLoading={loading} />
+        isLoading={loading}
+        isSaveHidden={isSaveHidden}
+        onChange={onChange}
+        onSave={save} />
     </Section>
   )
 }
