02ccfe5e751a0af859500721db1c914f480c8655
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-04 11:14:13 -0600

    [#FZC-I164] Added logic to update limit date on Submission detail page

--- pages/admin/submissions/[id]/index.js
+++ pages/admin/submissions/[id]/index.js
@@ -14,21 +14,54 @@ import {
   AdminSubmissionContext
 } from "../../../../contexts/admin/submissions/show"
 import { submission } from "../../../../graphql/submission"
-import { useMemo, useState } from "react"
-import { useQuery } from "@apollo/react-hooks"
+import { useCallback, useMemo, useState } from "react"
+import { useMutation, useQuery } from "@apollo/react-hooks"
 import { withApollo } from "../../../../helpers/withApollo"
 
 function Submission({ client }) {
   const router = useRouter()
-  const [ state ] = useState({
+  const [ state, setState ] = useState({
     submissionDetail: {}
   })
+
+  const [ updateSubmission ] = useMutation(
+    submission.mutations.updateById, { client: client }
+  )
+
   const { loading, error, data } = useQuery(submission.queries.getById, {
     client: client,
     variables: { id: router.query.id }
   })
 
+  const updateSubmissionDetail = useCallback(submission => {
+    const newSubmission = {
+      ...state.submissionDetail,
+      ...submission
+    }
+
+    setState({
+      ...state,
+      dirty: true,
+      submissionDetail: newSubmission
+    })
+  })
+
+  const save = useCallback(async () => {
+    try {
+      const updatedSubmission = await updateSubmission({
+        variables: { ...state.submissionDetail, id: router.query.id }
+      })
+
+      /* TODO: Show feedback to the user */
+    }
+    catch(e) {
+      console.error(e)
+    }
+  })
+
   const injectActions = useMemo(() => ({
+    updateSubmissionDetail,
+    save,
     loading,
     error,
     data
