3a4e74c5e7d132e4fa64f94ec5473767b33ce09c
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-06 14:26:11 -0600

    [#FZC-I212] Added logic to update contract sign date

--- components/admin/submissions/show/signed-agreement/index.js
+++ components/admin/submissions/show/signed-agreement/index.js
@@ -1,23 +1,36 @@
 import { Section } from "../../../../shared"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import {
   AdminSubmissionContext
 } from "../../../../../contexts/admin/submissions/show"
 import SubmissionAgreement from "./documents"
 
 export function SignedAgreement() {
+  const [ isSaveHidden, setIsSaveHidden] = useState(true)
   const {
+    updateSubmissionDetail,
+    save,
     loading,
     error,
     data
   } = useContext(AdminSubmissionContext)
+
+  const onChange = newContractSignDate => {
+    setIsSaveHidden(false)
+    const newSubmission = {...data?.Submission}
+    newSubmission.contractSignDate = newContractSignDate
+    updateSubmissionDetail(newSubmission)
+  }
 
   return (
     <Section title="Convenio firmado">
       <SubmissionAgreement
         data={data?.Submission}
         error={error}
-        isLoading={loading} />
+        isLoading={loading}
+        isSaveHidden={isSaveHidden}
+        onChange={onChange}
+        onSave={save}/>
     </Section>
   )
 }
