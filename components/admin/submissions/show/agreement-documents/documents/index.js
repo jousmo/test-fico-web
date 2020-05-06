25d2b5a09f046de4b72b25a1556f8f52ff913445
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-06 12:39:37 -0600

    [#FZC-I205] Added placeholder for submissions that are not on agreement

--- components/admin/submissions/show/agreement-documents/documents/index.js
+++ components/admin/submissions/show/agreement-documents/documents/index.js
@@ -1,5 +1,5 @@
 import { withForm } from "../../../../../../helpers/withForm"
-import { Col, Form, List, Row } from "antd"
+import { Col, Form, List, Row, Typography } from "antd"
 import { PaperClipOutlined } from "@ant-design/icons"
 import { DeleteButton } from "../../../../../shared"
 
@@ -7,47 +7,54 @@ function AgreementDocuments({ data }) {
 
   const getAction = () => {
     return [
-      <>
-        <a><PaperClipOutlined />&nbsp;documento.pdf</a>&nbsp;
-      </>,
+      <a><PaperClipOutlined />&nbsp;documento.pdf</a>,
       <DeleteButton style={{float: "none"}} />
     ]
   }
 
   return (
-    <Form
-      name="organizational-chart"
-      layout="vertical">
-      <Row gutter={[10, 8]} justify="start">
-        <Col span={24}>
-          <Form.Item
-            style={{display: "inline"}}
-            label="Documentos de la implementadora para firma de convenio">
-            <List bordered>
-              <List.Item actions={getAction()}>
-                Acta constitutiva
-              </List.Item>
-              <List.Item actions={getAction()}>
-                Poder representante legal
-              </List.Item>
-              <List.Item actions={getAction()}>
-                Copia de la identificación oficial de representante legan
-              </List.Item>
-              <List.Item actions={getAction()}>
-                Copia de constancia de situación
-              </List.Item>
-              <List.Item actions={getAction()}>
-                Comprobante de domicilio
-              </List.Item>
-              <List.Item actions={getAction()}>
-                Copia de la caratula del estado de cuenta (Cuenta exclusiva
-                de proyecto)
-              </List.Item>
-            </List>
-          </Form.Item>
-        </Col>
-      </Row>
-    </Form>
+    <div>
+      { data?.Submission?.status === "ON_AGREEMENT" ? (
+        <Form
+          name="organizational-chart"
+          layout="vertical">
+          <Row gutter={[10, 8]} justify="start">
+            <Col span={24}>
+              <Form.Item
+                style={{display: "inline"}}
+                label="Documentos de la implementadora para firma de convenio">
+                <List bordered>
+                  <List.Item actions={getAction()}>
+                    Acta constitutiva
+                  </List.Item>
+                  <List.Item actions={getAction()}>
+                    Poder representante legal
+                  </List.Item>
+                  <List.Item actions={getAction()}>
+                    Copia de la identificación oficial de representante legan
+                  </List.Item>
+                  <List.Item actions={getAction()}>
+                    Copia de constancia de situación
+                  </List.Item>
+                  <List.Item actions={getAction()}>
+                    Comprobante de domicilio
+                  </List.Item>
+                  <List.Item actions={getAction()}>
+                    Copia de la caratula del estado de cuenta (Cuenta exclusiva
+                    de proyecto)
+                  </List.Item>
+                </List>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      ) : (
+        <Typography.Text>
+          Cuando se apruebe el proyecto de la implementadora se podrá descargar
+          los anexos de la solicitud.
+        </Typography.Text>
+      )}
+    </div>
   )
 }
 
