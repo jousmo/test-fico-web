161648fb1b0ec2f47fb3bcfc8522f0b554aa8c2b
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-06 12:49:57 -0600

    [#FZC-I164] Changed name of properties

--- helpers/selectOptions/shared/statusOptions.js
+++ helpers/selectOptions/shared/statusOptions.js
@@ -1,9 +1,9 @@
 export const statusOptions = [
   { label: "Solicitud creada", value: "CREATED" },
   { label: "Revisión", value: "REVIEW" },
-  { label: "Revisión 1", value: "FIRST_FEEDBACK" },
-  { label: "Revisión 2", value: "SECOND_FEEDBACK" },
-  { label: "Revisión 3", value: "THIRD_FEEDBACK" },
+  { label: "Revisión 1", value: "REVIEW_1" },
+  { label: "Revisión 2", value: "REVIEW_2" },
+  { label: "Revisión 3", value: "REVIEW_3" },
   { label: "Solicitud rechazada", value: "REJECTED" },
   { label: "En convenio", value: "ON_AGREEMENT" },
   { label: "Aprobada", value: "APPROVED" }
