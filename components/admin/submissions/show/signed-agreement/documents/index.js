e2b732b96c9e9ab85d665492243eac49bf7ba1a9
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-06 14:24:03 -0600

    [#FZC-I212] Added contract signing datefield for submissions that are on status `ON_AGREEMENT`

--- components/admin/submissions/show/signed-agreement/documents/index.js
+++ components/admin/submissions/show/signed-agreement/documents/index.js
@@ -1,20 +1,44 @@
 import { withForm } from "../../../../../../helpers/withForm"
-import { Typography } from "antd"
-import { UploadButton } from "../../../../../shared"
+import { Button, Descriptions } from "antd"
+import { DateField, UploadButton } from "../../../../../shared"
+import { getSelectValue } from "../../../../../../helpers/getSelectValue"
 
-function SubmissionAgreement({ data }) {
+function SubmissionAgreement({ isSaveHidden, data, onChange, onSave }) {
+
+  const onDateChange = dateObject => {
+    const newSignDate = getSelectValue(dateObject)
+    onChange(newSignDate)
+  }
+
   return (
-    <div>
-      <Typography.Text>
+    <Descriptions>
+      <Descriptions.Item span={3}>
         Anexa el convenio firmado
-      </Typography.Text>
-      <br />
-      <UploadButton
-        style={{margin: "5px"}}
-        disabled={!data?.Submission?.contractSignDate}>
-        Subir convenio firmado
-      </UploadButton>
-    </div>
+      </Descriptions.Item>
+      { data?.Submission?.status === "ON_AGREEMENT" ? (
+        <Descriptions.Item label="Fecha de firma de convenio" span={3}>
+          <DateField
+            id="contractSignDate"
+            defaultValue={data?.contractSignDate}
+            onChange={onDateChange}
+            size="small" />
+          &nbsp;
+          <Button
+            onClick={onSave}
+            hidden={isSaveHidden}
+            size="small">
+            Guardar
+          </Button>
+        </Descriptions.Item>
+      ) : null }
+      <Descriptions.Item>
+        <UploadButton
+          style={{margin: "5px"}}
+          disabled={!data?.Submission?.contractSignDate}>
+          Subir convenio firmado
+        </UploadButton>
+      </Descriptions.Item>
+    </Descriptions>
   )
 }
 
