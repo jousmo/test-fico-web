a5a38c8a32379d287d5212f595ea6fd2a03580f6
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-07 10:39:15 -0600

    [#FZC-I205] Refactor after feedback - Changed name of component to AgreementDocumentsForm - Changed ternary operator for condition-return - Added Alert ant component

---
+++ components/admin/submissions/show/agreement-documents/form/index.js
@@ -0,0 +1,62 @@
+import { withForm } from "../../../../../../helpers/withForm"
+import { Alert, Col, Form, List, Row } from "antd"
+import { PaperClipOutlined } from "@ant-design/icons"
+import { DeleteButton } from "../../../../../shared"
+
+function AgreementDocumentsForm({ data }) {
+
+  const getAction = () => {
+    return [
+      <a><PaperClipOutlined />&nbsp;documento.pdf</a>,
+      <DeleteButton style={{float: "none"}} />
+    ]
+  }
+
+  if (data?.Submission?.status !== "ON_AGREEMENT"){
+    return (
+      <Alert
+        message="Cuando se apruebe el proyecto de la implementadora se podrá
+        descargar los anexos de la solicitud."
+        type="warning"
+      />
+    )
+  }
+
+  return (
+    <Form
+      name="organizational-chart"
+      layout="vertical">
+      <Row gutter={[10, 8]} justify="start">
+        <Col span={24}>
+          <Form.Item
+            style={{display: "inline"}}
+            label="Documentos de la implementadora para firma de convenio">
+            <List bordered>
+              <List.Item actions={getAction()}>
+                Acta constitutiva
+              </List.Item>
+              <List.Item actions={getAction()}>
+                Poder representante legal
+              </List.Item>
+              <List.Item actions={getAction()}>
+                Copia de la identificación oficial de representante legan
+              </List.Item>
+              <List.Item actions={getAction()}>
+                Copia de constancia de situación
+              </List.Item>
+              <List.Item actions={getAction()}>
+                Comprobante de domicilio
+              </List.Item>
+              <List.Item actions={getAction()}>
+                Copia de la caratula del estado de cuenta (Cuenta exclusiva
+                de proyecto)
+              </List.Item>
+            </List>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+  )
+}
+
+export default withForm(AgreementDocumentsForm)
