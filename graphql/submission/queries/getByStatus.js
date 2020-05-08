409bbbd442ba2d532b5993d90d8e4b7b4710bf04
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-08 16:01:50 -0600

    [#FZC-I204] Updated getByStatus query

--- graphql/submission/queries/getByStatus.js
+++ graphql/submission/queries/getByStatus.js
@@ -1,8 +1,8 @@
 import { gql } from "apollo-boost"
 
 export const getByStatus = gql`
-  query SubmissionByStatus($status: status) {
-    Submission(status: $status) {
+  query SubmissionByStatus($status: String) {
+    allSubmissions(filter: {status: $status}) {
       id
       name
     }
