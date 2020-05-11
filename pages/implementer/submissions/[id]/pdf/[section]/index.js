91954d20fb2e80d161236f6c2ea404b620cf517c
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-11 10:40:14 -0600

    [#FZC-I207] Added ViewPDF page

---
+++ pages/implementer/submissions/[id]/pdf/[section]/index.js
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router"
+import {
+  TechnicalSpecification
+} from "../../../../../../components/implementer/submissions/pdf"
+import {
+  ImplementerSubmissionContext
+} from "../../../../../../contexts/implementer/submissions/show"
+import { submission } from "../../../../../../graphql/submission"
+import { useMemo, useState } from "react"
+import { useQuery } from "@apollo/react-hooks"
+import { withApollo } from "../../../../../../helpers/withApollo"
+
+function ViewPDF({ client }) {
+  const router = useRouter()
+  const [ state ] = useState({
+    viewPDF: {}
+  })
+
+  const { loading, error, data } = useQuery(submission.queries.getById, {
+    client: client,
+    variables: { id: router.query.id }
+  })
+
+  const injectActions = useMemo(() => ({
+    loading,
+    error,
+    data
+  }), [state])
+
+  let sectionComponent
+  if (router.query.section === "technical-specification"){
+    sectionComponent = TechnicalSpecification
+  }
+
+  return (
+    <ImplementerSubmissionContext.Provider value={injectActions}>
+      {sectionComponent}
+    </ImplementerSubmissionContext.Provider>
+  )
+}
+
+export default withApollo(ViewPDF)
