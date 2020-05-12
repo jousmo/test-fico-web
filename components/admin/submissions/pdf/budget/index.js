fb3687e9039ba08a64cd7a4e17c61cec87e72d13
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-12 12:28:26 -0600

    [#FZC-I208] WIP: Added BudgetPDF component

---
+++ components/admin/submissions/pdf/budget/index.js
@@ -0,0 +1,49 @@
+import { Table } from "antd"
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../../contexts/admin/submissions/show"
+import { getReadableValue } from "../../../../../helpers/selectOptions"
+import {
+  conceptTypes
+} from "../../../../../helpers/selectOptions/implementer/submission"
+import PDFHeading from "../heading"
+import "../style.sass"
+
+export function BudgetPDF() {
+  const {
+    submissionResult
+  } = useContext(AdminSubmissionContext)
+
+  return (
+    <div className="fico pdf budget">
+      <PDFHeading title="Presupuesto" />
+      <Table
+        dataSource={submissionResult?.data?.Submission?.concepts}
+        pagination={false}
+        rowKey={(row, index) => index}>
+        <Table.Column
+          render={(text, row, index) => index + 1}
+          title="#" />
+        <Table.Column
+          dataIndex="name"
+          title="Concepto" />
+        <Table.Column
+          dataIndex="type"
+          render={text => getReadableValue(conceptTypes, text)}
+          title="Tipo" />
+        <Table.Column title="Aportaciones">
+          <Table.Column
+            title="FF AC" />
+          <Table.Column
+            title="FECHAC" />
+          <Table.Column
+            title="OSC" />
+          <Table.Column
+            render={(text, row) => row.unitCost * row.totalUnits}
+            title="TOTAL" />
+        </Table.Column>
+      </Table>
+    </div>
+  )
+}
--- components/admin/submissions/pdf/index.js
+++ components/admin/submissions/pdf/index.js
@@ -1,3 +1,4 @@
 export * from "./general-information"
 export * from "./reports"
 export * from "./technical-specification"
+export * from "./budget"
