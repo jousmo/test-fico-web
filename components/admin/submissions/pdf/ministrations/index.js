4dc5e23667893723a13041a6051ddd41a1421c94
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-18 14:13:36 -0600

    [#FZC-I210] Added logic to display dynamic columns

--- components/admin/submissions/pdf/ministrations/index.js
+++ components/admin/submissions/pdf/ministrations/index.js
@@ -1,3 +1,7 @@
+import { Table, Typography } from "antd"
+import Moment from "moment"
+import { extendMoment } from "moment-range"
+const moment = extendMoment(Moment)
 import { useContext } from "react"
 import {
   AdminSubmissionContext
@@ -9,10 +13,53 @@ export function MinistrationsPDF(){
   const {
     submissionResult
   } = useContext(AdminSubmissionContext)
+  const submission = submissionResult?.data?.Submission
+
+  const getPeriodTitle = (month, index) => {
+    return (
+      <>
+        <Typography.Text>
+          Periodo {index + 1}
+        </Typography.Text>
+        <br/>
+        <Typography.Text type="secondary">
+          {month.format("DD/MM/YYYY")} - {month.add(1, "month").format("DD/MM/YYYY")}
+        </Typography.Text>
+      </>
+    )
+  }
+
+  const range = moment.range(moment(submission?.startDate),
+    moment(submission?.endDate))
+  const years = Array.from(range.by("year"))
+  const months = Array.from(range.by("month"))
 
   return (
-    <div className="fico pdf reports">
+    <div className="fico pdf ministrations">
       <PDFHeading title="Ministración" />
+      { years.map((year, index) =>
+        <Table
+          dataSource={submission?.concepts}
+          key={index}
+          pagination={false}
+          rowKey={(row, index) => index}>
+          <Table.Column
+            title="Aportante" />
+          <Table.Column title={year.format("YYYY")}>
+            {months.map((month, index) => {
+              if (month.isSame(year, "year")){
+                return (
+                  <Table.Column
+                    key={index}
+                    title={getPeriodTitle(month, index)}/>
+                )
+              }
+            })}
+            <Table.Column
+              title="Total"/>
+          </Table.Column>
+        </Table>
+      )}
     </div>
   )
 }
