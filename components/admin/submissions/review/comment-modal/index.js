98bf5f6b61ad1ee84f96728a918f94b97e80da94
Rodrigo D. Cuervo <rod750@gmail.com>
2020-05-19 13:27:23 -0500

    [#FZC-I226] #comment Wrote comment modal component

---
+++ components/admin/submissions/review/comment-modal/index.js
@@ -0,0 +1,53 @@
+import { Modal, Form, Input } from "antd"
+import { useForm } from "antd/lib/form/util"
+import { useEffect } from "react"
+
+export function CommentModal({
+  onCancel,
+  onSave,
+  revision,
+  fieldName,
+  fieldSection,
+  ...props
+}) {
+  const [form] = useForm()
+
+  useEffect(() => {
+    /* Here use `revision`, `fieldSection`, `fieldName` to retrieve the saved
+    * comments for listing */
+  }, [revision, fieldName, fieldSection])
+
+  const onOk = async () => {
+    try {
+      const values = await form.getFieldsValue()
+      onSave(values)
+      form.resetFields()
+    }
+    catch(e) {
+      console.error(e)
+    }
+  }
+
+
+  return (
+    <Modal
+      title="Comentarios para revisión"
+      onOk={onOk}
+      okText="Guardar"
+      onCancel={onCancel}
+      cancelText="Cancelar"
+      width={800}
+      {...props}>
+      <Form
+        form={form}
+        layout="vertical">
+        <Form.Item
+          name="comment">
+          <Input.TextArea
+            id="comment"
+            placeholder="Describe los puntos a corregir" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
