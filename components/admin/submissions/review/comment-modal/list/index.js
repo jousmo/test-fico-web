d372d43eda4705cb58f1724e55cc49e189cdd5a4
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-20 17:03:11 -0600

    [#FZC-I176] Adds CommentListing component

---
+++ components/admin/submissions/review/comment-modal/list/index.js
@@ -0,0 +1,30 @@
+import { List, Typography } from "antd"
+import moment from "moment"
+import {
+  statusOptions
+} from "../../../../../../helpers/selectOptions/shared/statusOptions"
+import {
+  getReadableValue
+} from "../../../../../../helpers/selectOptions/getReadableValue"
+
+export function CommentListing({ comments }){
+  return (
+    <List
+      header={
+        <Typography.Text>Comentarios ({comments.length})</Typography.Text>
+      }>
+      {comments.map((element, index) =>
+        <List.Item key={index}>
+          <Typography.Text>
+            {element.comment}
+          </Typography.Text>
+          <Typography.Text type="secondary">
+            {getReadableValue(statusOptions, element.revision)} -
+            &nbsp;
+            {moment(element.createdAt).format("DD/MM/YYYY HH:MM")}
+          </Typography.Text>
+        </List.Item>
+      )}
+    </List>
+  )
+}
