e82f0949c44fd5375e1ea59b79078c8bd6e11a3b
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-22 12:15:39 -0600

    [#FZC-I175] Adds DeleteButton to comments from the same revision

--- components/admin/submissions/review/comment-modal/list/index.js
+++ components/admin/submissions/review/comment-modal/list/index.js
@@ -6,30 +6,37 @@ import {
 import {
   getReadableValue
 } from "../../../../../../helpers/selectOptions/getReadableValue"
+import { DeleteButton } from "../../../../../shared/delete-button"
 
-export function CommentListing({ comments }){
+export function CommentListing({ comments, onDelete, revision }){
   return (
     <List
       header={
         <Typography.Text>Comentarios ({comments?.length || 0})</Typography.Text>
       }>
-      {comments?.map((element, index) =>
-        <List.Item key={index}>
-          <List.Item.Meta
-            title={
-              <Typography.Text>
-                {element.comment}
-              </Typography.Text>
-            }
-            description={
-              <Typography.Text type="secondary">
-                {getReadableValue(statusOptions, element.revision)} -
-                &nbsp;
-                {moment(element.createdAt).format("DD/MM/YYYY HH:MM")}
-              </Typography.Text>
-            } />
-        </List.Item>
-      )}
+      {comments?.map((element, index) => {
+        let action = []
+        if (element.revision === revision){
+          action.push(<DeleteButton onClick={() => onDelete(index)}/>)
+        }
+        return (
+          <List.Item key={index} actions={action}>
+            <List.Item.Meta
+              title={
+                <Typography.Text>
+                  {element.comment}
+                </Typography.Text>
+              }
+              description={
+                <Typography.Text type="secondary">
+                  {getReadableValue(statusOptions, element.revision)} -
+                  &nbsp;
+                  {moment(element.createdAt).format("DD/MM/YYYY HH:MM")}
+                </Typography.Text>
+              } />
+          </List.Item>
+        )
+      })}
     </List>
   )
 }
