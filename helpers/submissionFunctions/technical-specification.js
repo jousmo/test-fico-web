d686b3af88751e7f7ea923c7b9caf2e8e242106a
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-22 15:27:15 -0600

    [#FZC-I192] Adds helper functions for technical specification

---
+++ helpers/submissionFunctions/technical-specification.js
@@ -0,0 +1,26 @@
+
+export const setUpdateTechnicalSpecification = (technicalSpecification, state, setState) => {
+  const newTechnicalSpecification = {
+    ...state.technicalSpecification,
+    ...technicalSpecification
+  }
+
+  setState({
+    ...state,
+    dirty: true,
+    technicalSpecification: newTechnicalSpecification
+  })
+}
+
+export const setSave = async (state, updateSubmission, id) => {
+  try {
+    const updatedSubmission = await updateSubmission({
+      variables: { ...state.technicalSpecification, id: id }
+    })
+
+    /* TODO: Show feedback to the user */
+  }
+  catch(e) {
+    console.error(e)
+  }
+}
