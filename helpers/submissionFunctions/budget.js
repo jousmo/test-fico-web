b45243e09c3899fc36b441f76b53aee416a6b702
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-26 17:10:02 -0600

    [#FZC-I198] Adds budget submission helper functions

---
+++ helpers/submissionFunctions/budget.js
@@ -0,0 +1,26 @@
+
+export const setUpdateBudget = (budget, state, setState) => {
+  const newBudget = {
+    ...state.budget,
+    ...budget
+  }
+
+  setState({
+    ...state,
+    dirty: true,
+    budget: newBudget
+  })
+}
+
+export const setSave = async (state, updateSubmission, id) => {
+  try {
+    const updatedSubmission = await updateSubmission({
+      variables: { ...state.budget, id: id }
+    })
+
+    /* TODO: Show feedback to the user */
+  }
+  catch(e) {
+    console.error(e)
+  }
+}
