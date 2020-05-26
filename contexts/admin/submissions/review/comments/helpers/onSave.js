cc55ee8b32a05c0620f71c7f37b66822bfd1ab99
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-05-26 10:21:06 -0600

    [#FZC-I201] Extracted onSave function to helpers

---
+++ contexts/admin/submissions/review/comments/helpers/onSave.js
@@ -0,0 +1,55 @@
+export const onSaveHelper = (comment, index, section, submission, update) => {
+  switch (section){
+    case "submission":
+      const submissionComments = submission?.comments
+      const newSubmissionComments = [
+        ...submissionComments,
+        comment
+      ]
+      update({ comments: newSubmissionComments })
+      break
+    case "consultant":
+      const consultant = submission?.consultant
+      const consultantComments = consultant?.comments || []
+      const newConsultantComments = [
+        ...consultantComments,
+        comment
+      ]
+      const newConsultant = {
+        ...consultant,
+        comments: newConsultantComments
+      }
+      update({ consultant: newConsultant })
+      break
+    case "generalIndicator":
+      const generalIndicators =
+        [...submission?.generalObjectiveIndicators]
+      const generalIndicator = generalIndicators[index]
+      const generalIndicatorComments = generalIndicator?.comments || []
+      const generalComments = [
+        ...generalIndicatorComments,
+        comment
+      ]
+      generalIndicators[index] = {
+        ...generalIndicator,
+        comments: generalComments
+      }
+      update({ generalObjectiveIndicators: generalIndicators })
+      break
+    case "developmentIndicator":
+      const developmentIndicators =
+        [...submission?.developmentObjectiveIndicators]
+      const developmentIndicator = developmentIndicators[index]
+      const developmentIndicatorComments = developmentIndicator?.comments || []
+      const developmentComments = [
+        ...developmentIndicatorComments,
+        comment
+      ]
+      developmentIndicators[index] = {
+        ...developmentIndicator,
+        comments: developmentComments
+      }
+      update({ developmentObjectiveIndicators: developmentIndicators })
+      break
+  }
+}
