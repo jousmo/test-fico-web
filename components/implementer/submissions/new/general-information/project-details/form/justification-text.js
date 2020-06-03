4c6e7a156ea1d565a48a682c1084f4930ff14804
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-03 13:02:46 -0600

    [#FZC-I230] Adds JustificationText

---
+++ components/implementer/submissions/new/general-information/project-details/form/justification-text.js
@@ -0,0 +1,13 @@
+import { Typography } from "antd"
+
+export function JustificationText() {
+  return (
+    <div style={{maxWidth: "400px", padding: "10px"}}>
+      <Typography.Text>
+        Basarse en datos estadísticos, indicando la fuente de información.
+        Dar preferencia a datos proporcionados por el observatorio ciudadano
+        de FICOSEC.
+      </Typography.Text>
+    </div>
+  )
+}
