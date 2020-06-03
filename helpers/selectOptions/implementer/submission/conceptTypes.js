677ee36edcf438ec4619e5ffa87d76b8b35db4e2
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-03 10:27:53 -0600

    [#FZC-I235] Updates conceptTypes options

--- helpers/selectOptions/implementer/submission/conceptTypes.js
+++ helpers/selectOptions/implementer/submission/conceptTypes.js
@@ -1,11 +1,12 @@
 export const conceptTypes = [
-  { label: "Recurso humano", value: "HUMAN_RESOURCE" },
-  { label: "Materiales y suministros", value: "SUPPLIES" },
+  { label: "Capacitación a personal y capacitación a beneficiario", value: "TRAINING" },
   { label: "Equipamiento", value: "EQUIPMENT" },
-  { label: "Gastos de oficina", value: "OFFICE_EXPENSES" },
-  { label: "Seguimiento y evaluación", value: "MONITORING" },
-  { label: "Promoción y publicidad", value: "MARKETING" },
   { label: "Eventos", value: "EVENTS" },
-  { label: "Capacitación", value: "TRAINING" },
-  { label: "Gastos financieros", value: "FINANCIAL_EXPENSES" }
+  { label: "Gastos administrativos", value: "ADMINISTRATIVE_EXPENSES" },
+  { label: "Gastos de viaje", value: "TRAVEL_EXPENSES" },
+  { label: "Gastos financieros", value: "FINANCIAL_EXPENSES" },
+  { label: "Materiales y suministros", value: "SUPPLIES" },
+  { label: "Promoción y publicidad", value: "MARKETING" },
+  { label: "Recurso humano", value: "HUMAN_RESOURCE" },
+  { label: "Seguimiento y evaluación", value: "MONITORING" }
 ]
