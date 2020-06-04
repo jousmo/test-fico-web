54ef58e768e416efbecc387effa430d6c343b831
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-04 17:07:49 -0600

    [#FZC-I144] Adds townships select options

--- helpers/selectOptions/implementer/submission/regions.js
+++ helpers/selectOptions/implementer/submission/regions.js
@@ -4,3 +4,82 @@ export const regions = [
   { label: "Cuauhtémoc", value: "Cuauhtemoc" },
   { label: "Chihuahua", value: "Chihuahua" }
 ]
+
+export const townships = [
+  {
+    region: "Parral",
+    townships: [
+      "Allende",
+      "Balleza",
+      "Batopilas",
+      "Coronado",
+      "El Tule",
+      "Guachochi",
+      "Guadalupe y Calvo",
+      "Hidalgo del Parral",
+      "Huejotitan",
+      "Jiménez",
+      "Lopez",
+      "Matamoros",
+      "Morales",
+      "Rosario",
+      "San Francisco del Oro",
+      "Santa Bárbara",
+      "Valle de Zaragoza"
+    ]
+  },
+  {
+    region: "Delicias",
+    townships: [
+      "Camargo",
+      "Delicias",
+      "Julimes",
+      "La Cruz",
+      "Meoqui",
+      "Rosales",
+      "Saucillo",
+      "San Francisco de Conchos"
+    ]
+  },
+  {
+    region: "Cuauhtémoc",
+    townships: [
+      "Bachiniva",
+      "Bocoyna",
+      "Carichi",
+      "Chínipas",
+      "Cuauhtémoc",
+      "Cusihuirachi",
+      "Gómez Farías",
+      "Guazaparez",
+      "Guerrero",
+      "Madera",
+      "Maguarichi",
+      "Matachic",
+      "Moris",
+      "Namiquipa",
+      "Nonoava",
+      "Ocampo",
+      "San Francisco de Borja",
+      "Temosachic",
+      "Urique",
+      "Uruachi"
+    ]
+  },
+  {
+    region: "Chihuahua",
+    townships: [
+      "Aldama",
+      "Aquiles Serdán",
+      "Chihuahua",
+      "Coyame del Sotol",
+      "Dr. Belisario Domínguez",
+      "Gran Morelos",
+      "Manuel Benavides",
+      "Ojinaga",
+      "Riva Palacio",
+      "Santa Isabel",
+      "Satevó"
+    ]
+  }
+]
