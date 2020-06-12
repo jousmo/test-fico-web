131265a0d9e0703021f625c65881ec374a73d3e8
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-12 13:16:35 -0600

    [#FZC-I251] Updates onDeleteHelper to handle multiple consultant comments

--- contexts/admin/submissions/review/comments/helpers/sections/consultant.js
+++ contexts/admin/submissions/review/comments/helpers/sections/consultant.js
@@ -1,12 +1,14 @@
-export const deleteConsultantComments = (submission, toDelete) => {
-  const consultant = submission?.consultant
+export const deleteConsultantComments = (submission, toDelete, index) => {
+  const consultants = [...submission?.consultants]
+  const consultant = consultants[index]
   const newComments = consultant?.comments?.filter(e =>
     (e.comment !== toDelete.comment && e.createdAt !== toDelete.createdAt)
   )
-  return {
+  consultants[index] = {
     ...consultant,
     comments: newComments
   }
+  return consultants
 }
 
 export const addConsultantComment = (submission, comment, index) => {
