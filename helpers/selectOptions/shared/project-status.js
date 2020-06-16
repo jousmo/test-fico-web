dd1c391500dd892fd85f648f22813a1a9567652f
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-16 12:57:59 -0600

    [#FZC-I254] Adds projectStatusOptions helper

---
+++ helpers/selectOptions/shared/project-status.js
@@ -0,0 +1,12 @@
+export const projectStatusOptions = [
+  { label: "Enviar información para monitoreo", value: "AWAITING_INFO" },
+  { label: "Información enviada", value: "INFO_SENT" },
+  { label: "Monitoreo en proceso", value: "ON_MONITORING" },
+  { label: "Observaciones de monitoreo emitidas", value: "OBSERVATIONS_SENT" },
+  {
+    label: "Observaciones de monitoreo atendidas",
+    value: "OBSERVATIONS_REPLIED"
+  },
+  { label: "En evaluación", value: "ON_EVALUATION" },
+  { label: "En cierre", value: "ON_CLOSURE" }
+]
