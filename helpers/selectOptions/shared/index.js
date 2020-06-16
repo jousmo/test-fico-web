be92f542a6cf786880d3fd559be69f3d164f3896
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-16 12:41:19 -0600

    [#FZC-I254] Adds submission state options

--- helpers/selectOptions/shared/index.js
+++ helpers/selectOptions/shared/index.js
@@ -1,4 +1,5 @@
 import { yesNo } from "./yesNo"
 import { statusOptions } from "./statusOptions"
+import { stateOption } from "./project-state"
 
-export const shared = { statusOptions, yesNo }
+export const shared = { stateOption, statusOptions, yesNo }
