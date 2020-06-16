888ad87ee6c4fa2b85c047fb41c7fe9c73aa3719
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-16 14:23:26 -0600

    [#FZC-I254] Adds getTag helper function

---
+++ components/admin/projects/list/table/helpers/get-tag.js
@@ -0,0 +1,59 @@
+import {
+  getReadableValue
+} from "../../../../../../helpers/selectOptions/getReadableValue"
+import {
+  CheckCircleOutlined,
+  ClockCircleOutlined,
+  CloseCircleOutlined,
+  SyncOutlined
+} from "@ant-design/icons"
+import { Tag } from "antd"
+
+export const getTag = (value, options) => {
+  const tagSetting = {}
+
+  switch (value) {
+    case "AWAITING_INFO": {
+      tagSetting.color = "cyan"
+      tagSetting.icon = <ClockCircleOutlined />
+      break
+    }
+    case "INFO_SENT": {
+      tagSetting.color = "magenta"
+      tagSetting.icon = <CheckCircleOutlined />
+      break
+    }
+    case "ON_MONITORING": {
+      tagSetting.color = "blue"
+      tagSetting.icon = <SyncOutlined />
+      break
+    }
+    case "OBSERVATIONS_SENT": {
+      tagSetting.color = "purple"
+      tagSetting.icon = <ClockCircleOutlined />
+      break
+    }
+    case "OBSERVATIONS_REPLIED": {
+      tagSetting.color = "green"
+      tagSetting.icon = <CheckCircleOutlined />
+      break
+    }
+    case "ON_EVALUATION": {
+      tagSetting.color = "orange"
+      tagSetting.icon = <ClockCircleOutlined />
+      break
+    }
+    case "ON_CLOSURE": {
+      tagSetting.color = "red"
+      tagSetting.icon = <CloseCircleOutlined />
+      break
+    }
+    default:
+      break
+  }
+  return (
+    <Tag color={tagSetting.color}>
+      {tagSetting.icon}&nbsp;{getReadableValue(options, value)}
+    </Tag>
+  )
+}
