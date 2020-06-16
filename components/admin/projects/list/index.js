d9573b798b1abbc65e6549ced33e12c87532db3e
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-16 17:01:52 -0600

    [#FZC-I254] Adds ProjectListing component

---
+++ components/admin/projects/list/index.js
@@ -0,0 +1,24 @@
+import { SearchField, Section } from "../../../shared"
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../contexts/admin/submissions/show"
+import ProjectListingTable from "./table"
+
+export function ProjectListing() {
+  const {
+    loading,
+    error,
+    data
+  } = useContext(AdminSubmissionContext)
+
+  return (
+    <Section style={{padding: 0}}>
+      <SearchField />
+      <ProjectListingTable
+        data={data?.allSubmissions}
+        error={error}
+        loading={loading} />
+    </Section>
+  )
+}
