3ead6942a3133ad83bb08e1b0be691da48648b74
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-17 12:03:22 -0600

    [#FZC-I255] Adds ProjectSummary component

---
+++ components/admin/projects/show/summary/index.js
@@ -0,0 +1,57 @@
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../../contexts/admin/submissions/show"
+import { Button, Col, Descriptions } from "antd"
+import { EyeOutlined } from "@ant-design/icons"
+import Link from "next/link"
+import {
+  DownloadOutlined,
+} from "@ant-design/icons"
+import { BreadcrumbHeading } from "../../../../shared/breadcrum-heading"
+import SummaryBody from "../../../../shared/project-summary-body"
+import { getTag } from "../../list/table/helpers"
+import "./style.sass"
+
+export function ProjectSummary() {
+  const {
+    loading,
+    error,
+    data
+  } = useContext(AdminSubmissionContext)
+
+  const headingButtons = (
+    <Col>
+      <Button ghost shape="circle" icon={<DownloadOutlined />} />
+      <Button className="end-button">Concluir proyecto</Button>
+    </Col>
+  )
+
+  const goToProject = (
+    <Descriptions.Item>
+      <Link href={`/admin/projects/${data?.Submission.id}/`}>
+        <a><EyeOutlined /> Ver la solicitud</a>
+      </Link>
+    </Descriptions.Item>
+  )
+
+  return (
+    <div className="fico project summary">
+      <BreadcrumbHeading
+        home={{ label: "Monitoreo", url: "/admin/projects" }}
+        itemsList={[
+          {
+            label: data?.Submission?.name,
+            url: `/admin/projects/${data?.Submission?.id}`
+          }
+        ]}
+        extra={headingButtons} />
+      <SummaryBody
+        data={data?.Submission}
+        error={error}
+        extra={goToProject}
+        getTag={getTag}
+        isLoading={loading} />
+    </div>
+  )
+}
