612aeeb0867b2c7bef02aa97d9e05b1e3a1a7ebf
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-22 17:00:55 -0600

    [#FZC-I259] Adds ProjectClosureForm component

---
+++ components/admin/projects/show/closure/form/index.js
@@ -0,0 +1,19 @@
+import { withForm } from "../../../../../../helpers/withForm"
+import { Typography } from "antd"
+import { UploadButton } from "../../../../../shared/upload-button"
+
+function ProjectClosureForm({ data }) {
+  return (
+    <div>
+      <Typography.Text>
+        Subir el documento firmado de cierro de proyecto en formato .PDF
+      </Typography.Text>
+      <br />
+      <UploadButton style={{ marginTop: "5px" }}>
+        Subir
+      </UploadButton>
+    </div>
+  )
+}
+
+export default withForm(ProjectClosureForm)
