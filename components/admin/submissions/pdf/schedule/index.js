967d86dddb8fa633d436d8b25ae7bacf8808e4de
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-24 17:25:06 -0600

    [#FZC-I209] Adds admin context to SchedulePDF

--- components/admin/submissions/pdf/schedule/index.js
+++ components/admin/submissions/pdf/schedule/index.js
@@ -1,7 +1,16 @@
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../../contexts/admin/submissions/show"
 import PDFHeading from "../heading"
 import "../style.sass"
 
 export function SchedulePDF(){
+  const {
+    submissionResult
+  } = useContext(AdminSubmissionContext)
+  const submission = submissionResult?.data?.Submission
+
   return (
     <div className="fico pdf schedule">
       <PDFHeading title="Cronograma" />
