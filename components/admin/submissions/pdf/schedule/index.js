d592fef096e6a5c2120559aba4eed712a7278c9a
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-06-25 11:52:43 -0600

    [#FZC-I209] Adds ant table to SchedulePDF

--- components/admin/submissions/pdf/schedule/index.js
+++ components/admin/submissions/pdf/schedule/index.js
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { Table } from "antd"
 import {
   AdminSubmissionContext
 } from "../../../../../contexts/admin/submissions/show"
@@ -14,6 +15,11 @@ export function SchedulePDF(){
   return (
     <div className="fico pdf schedule">
       <PDFHeading title="Cronograma" />
+      <Table bordered dataSource={dataSource} pagination={false}>
+        <Table.Column
+          dataIndex="activity"
+          title="Actividad"/>
+      </Table>
     </div>
   )
 }
