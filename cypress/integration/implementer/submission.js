67562f2376f40e5b96dab505b17000d8b3eeb4ab
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-06 17:30:46 -0600

    [#FZC-I277] Adds implementer submission page header tests

--- cypress/integration/implementer/submission.js
+++ cypress/integration/implementer/submission.js
@@ -6,4 +6,22 @@ describe("Implementer submission page", () => {
     cy.log("Visiting http://localhost:3000/implementer/submissions/1")
     cy.visit("/implementer/submissions/1")
   })
+
+  /** Header section **/
+  it("Should have user header", () => {
+    cy.get(".ant-layout-header")
+      .get(".ant-page-header-heading")
+      .get("#user_avatar")
+      .should("have.length", 1)
+
+    cy.get(".ant-layout-header")
+      .get(".ant-page-header-heading-extra")
+      .get("#search")
+      .should("have.length", 1)
+
+    cy.get(".ant-layout-header")
+      .get(".ant-page-header-heading-extra")
+      .get("#notifications")
+      .should("have.length", 1)
+  })
 })
