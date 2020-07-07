526e6763e288e7cd7df798b93411d56a5f2873a0
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-06 18:14:05 -0600

    [#FZC-I277] Adds new submission general information steps bar tests

--- cypress/integration/implementer/submissions/new/general-information.js
+++ cypress/integration/implementer/submissions/new/general-information.js
@@ -7,4 +7,17 @@ describe("Submission new general information page", () => {
     /new/general-information`)
     cy.visit("/implementer/submissions/new/general-information")
   })
+
+  it("Should have steps bar", () => {
+    cy.get(".ant-layout-content")
+      .get(".ant-page-header-content")
+      .get(".ant-page-header-heading-title")
+      .should("have.text", "Nueva solicitud")
+
+    cy.get(".ant-layout-content")
+      .get(".ant-page-header-content")
+      .get(".ant-steps > .ant-steps-item")
+      .eq(0)
+      .should("have.class", "ant-steps-item-active")
+  })
 })
