427996e0bd8af5eee0bc4f01555d119026e6c88e
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-07 13:11:01 -0600

    [#FZC-I278] Adds SubmissionsListing component

---
+++ components/admin/submissions/list/index.js
@@ -0,0 +1,24 @@
+import { SearchField, Section } from "../../../shared"
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../contexts/admin/submissions/show"
+import SubmissionsListingTable from "./table"
+
+export function SubmissionsListing() {
+  const {
+    loading,
+    error,
+    data
+  } = useContext(AdminSubmissionContext)
+
+  return (
+    <Section style={{padding: 0}}>
+      <SearchField />
+      <SubmissionsListingTable
+        data={data?.allSubmissions}
+        error={error}
+        isLoading={loading} />
+    </Section>
+  )
+}
