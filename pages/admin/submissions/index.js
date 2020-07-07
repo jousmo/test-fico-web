6292359763271afbbbc89dcc3218f84743840b25
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-07 10:08:40 -0600

    [#FZC-I278] Adds layout to admin submission list page

---
+++ pages/admin/submissions/index.js
@@ -0,0 +1,34 @@
+import { Layout } from "../../../components/shared"
+import {
+  AdminSubmissionContext
+} from "../../../contexts/admin/submissions/show"
+import { submission } from "../../../graphql/submission"
+import { useMemo, useState } from "react"
+import { useQuery } from "@apollo/react-hooks"
+import { withApollo } from "../../../helpers/withApollo"
+
+function AdminSubmissions({ client }) {
+  const [ state ] = useState({
+    submissionsList: {}
+  })
+
+  const { loading, error, data } = useQuery(submission.queries.getAll, {
+    client: client
+  })
+
+  const injectActions = useMemo(() => ({
+    loading,
+    error,
+    data
+  }), [state, loading])
+
+  return (
+    <AdminSubmissionContext.Provider value={injectActions}>
+      <Layout subheader={false}>
+
+      </Layout>
+    </AdminSubmissionContext.Provider>
+  )
+}
+
+export default withApollo(AdminSubmissions)
