4c81ae0b688d6f53aa03798dc48d6aaa41c97c1e
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-08 17:36:53 -0600

    [#FZC-I281] Implements ImplementerMenu component

--- components/shared/main-menu/index.js
+++ components/shared/main-menu/index.js
@@ -2,13 +2,13 @@ import "./style.sass"
 import { useContext } from "react"
 import { PageContext } from "../../../contexts/page"
 import AdminMenu from "./admin-menu"
+import ImplementerMenu from "./implementer-menu"
 
 export function MainMenu() {
   const { type, step, submenu } = useContext(PageContext)
 
   if (type === "implementer"){
-    //Todo: Return Implementer menu
-    return
+    return <ImplementerMenu step={step} submenu={submenu} />
   }
 
   return <AdminMenu step={step} submenu={submenu} />
