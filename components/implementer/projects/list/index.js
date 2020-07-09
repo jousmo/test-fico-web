abeaedbafea19b5e9b696dc3854f2aba66642168
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-09 12:09:17 -0600

    [#FZC-I282] Adds implementer ProjectListing component

---
+++ components/implementer/projects/list/index.js
@@ -0,0 +1,24 @@
+import { SearchField, Section } from "../../../shared"
+import { useContext } from "react"
+import {
+  ImplementerSubmissionContext
+} from "../../../../contexts/implementer/submissions/show"
+import ProjectListingTable from "./table"
+
+export function ProjectListing() {
+  const {
+    loading,
+    error,
+    data
+  } = useContext(ImplementerSubmissionContext)
+
+  return (
+    <Section style={{padding: 0}}>
+      <SearchField />
+      <ProjectListingTable
+        data={data?.allSubmissions}
+        error={error}
+        isLoading={loading} />
+    </Section>
+  )
+}
