3fd191c1ad14223d4e24f28bb7d13f2520908985
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-09 13:17:48 -0600

    [#FZC-I282] Adds implementer ProjectsList page

---
+++ pages/implementer/projects/index.js
@@ -0,0 +1,42 @@
+import { Layout } from "../../../components/shared"
+import {
+  ProjectListing
+} from "../../../components/implementer/projects/list"
+import {
+  ImplementerSubmissionContext
+} from "../../../contexts/implementer/submissions/show"
+import { PageContext } from "../../../contexts/page"
+import { submission } from "../../../graphql/submission"
+import { useMemo, useState } from "react"
+import { useQuery } from "@apollo/react-hooks"
+import { withApollo } from "../../../helpers/withApollo"
+
+function ProjectsList({ client }) {
+  const [ state ] = useState({
+    projects: {}
+  })
+
+  const { loading, error, data } = useQuery(submission.queries.getAll, {
+    client: client,
+    variables: { state: "PROJECT" }
+  })
+
+  const injectActions = useMemo(() => ({
+    loading,
+    error,
+    data
+  }), [state, loading])
+
+  return (
+    <PageContext.Provider
+      value={{ type: "submissions", step: "active", submenu: "projects" }}>
+      <ImplementerSubmissionContext.Provider value={injectActions}>
+        <Layout subheader={false}>
+          <ProjectListing />
+        </Layout>
+      </ImplementerSubmissionContext.Provider>
+    </PageContext.Provider>
+  )
+}
+
+export default withApollo(ProjectsList)
