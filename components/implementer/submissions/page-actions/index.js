a43ee52f5b4b9ed2809e199f3684bb385da073d9
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-20 12:19:55 -0600

    [#FZC-I289] Updates PageActions to handle close button

--- components/implementer/submissions/page-actions/index.js
+++ components/implementer/submissions/page-actions/index.js
@@ -1,10 +1,29 @@
 import { Button, Row, Col } from "antd"
 import { SaveOutlined } from "@ant-design/icons"
+import { useContext } from "react"
+import {
+  ImplementerSubmissionContext
+} from "../../../../contexts/implementer/submissions/new/context"
 
 export function PageActions({save}) {
+  const {
+    router
+  } = useContext(ImplementerSubmissionContext)
+
+  const handleClose = () => {
+    const { route } = router
+    let role = undefined
+    if (route.includes("implementer")) {
+      role = "implementer"
+    } else {
+      role = "admin"
+    }
+    router.push(`/${role}/submissions`)
+  }
+
   return (
     <Row justify="end" gutter={10}>
-      <Col><Button ghost>Cerrar</Button></Col>
+      <Col><Button ghost onClick={handleClose}>Cerrar</Button></Col>
       <Col>
         <Button
           icon={<SaveOutlined />}
