98d568cf22e9cdcafb4c591908aaeea50e14d160
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-21 14:42:18 -0600

    [#FZC-I290] Updates submissionStatus select options

--- helpers/selectOptions/shared/submission-status.js
+++ helpers/selectOptions/shared/submission-status.js
@@ -5,6 +5,8 @@ export const submissionStatusOptions = [
   { label: "Revisión 2", value: "REVIEW_2" },
   { label: "Revisión 3", value: "REVIEW_3" },
   { label: "Solicitud rechazada", value: "REJECTED" },
+  { label: "En consejo directivo", value: "ON_COUNCIL" },
+  { label: "En comité técnico", value: "ON_COMMITTEE" },
   { label: "En convenio", value: "ON_AGREEMENT" },
   { label: "Aprobada", value: "APPROVED" }
 ]
