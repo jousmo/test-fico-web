8abe15bdd511f9fe68db4ee79b9d2a524b732a8b
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-21 15:40:56 -0600

    [#FZC-I290] Updates EvaluationActionButtons to handle submission view

--- components/admin/submissions/status/evaluation-table/action-buttons/index.js
+++ components/admin/submissions/status/evaluation-table/action-buttons/index.js
@@ -1,4 +1,8 @@
+import { useContext } from "react"
 import { Button } from "antd"
+import {
+  AdminSubmissionContext
+} from "../../../../../../contexts/admin/submissions/show"
 import {
   CheckOutlined,
   CloseOutlined,
@@ -6,10 +10,19 @@ import {
 } from "@ant-design/icons"
 
 export function EvaluationActionButtons({ id }){
+  const {
+    router
+  } = useContext(AdminSubmissionContext)
+
+  const handleView = () => {
+    router.push(`/admin/submissions/${id}/review/general-information`)
+  }
+
   return (
     <div>
       <Button
         icon={<EyeOutlined />}
+        onClick={handleView}
         shape="circle" />
       &nbsp;
       <Button
