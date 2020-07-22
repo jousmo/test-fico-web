1dea44097ef6ef4dd3dc23b1d98c4282e64b3e12
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-22 17:15:17 -0600

    [#FZC-I302] Adds Login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,11 +4,23 @@ import {
 import {
   AuthSubmissionContext
 } from "../../contexts/auth"
+import { useMemo } from "react"
+import { useQuery } from "@apollo/react-hooks"
+import { submission } from "../../graphql/submission"
 import { withApollo } from "../../helpers/withApollo"
 
 function Login({ client }) {
+
+  const login = async (values) => {
+    // Todo: Update login logic once server authentication is running
+  }
+
+  const injectActions = useMemo(() => ({
+    login
+  }), [])
+
   return (
-    <AuthSubmissionContext.Provider value={[]}>
+    <AuthSubmissionContext.Provider value={injectActions}>
       <LoginContainer />
     </AuthSubmissionContext.Provider>
   )
