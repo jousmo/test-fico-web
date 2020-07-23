99d294001b2f6704aae5b661efa4f578b74a2543
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-23 10:52:40 -0600

    [#FZC-I291] Updates GovernmentList to get charge's readable value

--- components/admin/projects/show/government/list/index.js
+++ components/admin/projects/show/government/list/index.js
@@ -1,15 +1,22 @@
 import { withForm } from "../../../../../../helpers"
 import { List } from "antd"
+import {
+  getReadableValue,
+  implementer
+} from "../../../../../../helpers/selectOptions"
 
 function GovernmentList({ data }){
   const { councilMembers } = data?.implementer || {}
+  const {
+    profile: { chargeTypes }
+  } = implementer
 
   return (
     <List>
       <ul>
         {councilMembers?.map(({ name, charge }) => (
           <List.Item>
-            <li>{name} - {charge}</li>
+            <li>{name} - {getReadableValue(chargeTypes, charge)}</li>
           </List.Item>
         ))}
       </ul>
