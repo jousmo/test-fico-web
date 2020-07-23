60c1f723ec54f568b8187d47053dbf1fdb8600cc
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-23 16:50:01 -0600

    [#FZC-I295] Updates implementer profile update mutation

--- graphql/implementer/mutations/updateById.js
+++ graphql/implementer/mutations/updateById.js
@@ -1,52 +1,12 @@
-import { gql } from "apollo-boost";
+import { gql } from "apollo-boost"
 
 export const updateById = gql`
   mutation UpdateImplementerById(
     $id: ID!,
-    $type: String,
-    $name: String,
-    $director: String,
-    $rfc: String,
-    $commercialName: String,
-    $commercialAddress: String,
-    $fiscalAddress: String,
-    $phone: String,
-    $proofOfCharitableContributions: String,
-    $legalRepresentative: String,
-    $email: String,
-    $mission: String,
-    $vision: String,
-    $history: String,
-    $institutionalExperience: String,
-    $previousSupports: String,
-    $alliances: String,
-    $incomesAndExpenses: String,
-    $councilMembers: JSON,
-    $projects: JSON){
-      UpdateImplementer(
-        id: $id,
-        type: $type,
-        name: $name,
-        director: $director,
-        rfc: $rfc,
-        commercialName: $commercialName,
-        commercialAddress: $commercialAddress,
-        fiscalAddress: $fiscalAddress,
-        phone: $phone,
-        proofOfCharitableContributions: $proofOfCharitableContributions,
-        legalRepresentative: $legalRepresentative,
-        email: $email,
-        mission: $mission,
-        vision: $vision,
-        history: $history,
-        institutionalExperience: $institutionalExperience,
-        previousSupports: $previousSupports,
-        alliances: $alliances,
-        incomesAndExpenses: $incomesAndExpenses,
-        councilMembers: $councilMembers,
-        projects: $projects
-      ) {
-        id
-      }
+    $data: CreateImplementer!
+  ){
+    UpdateImplementer(id: $id, data: $data) {
+      id
+    }
   }
 `
