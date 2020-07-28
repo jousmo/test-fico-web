cf6e0db7e83a1a78a1a571120c11e5bcb6d824c7
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-07-28 10:55:01 -0600

    [#FZC-I290] Updates EvaluationTable to show technical opinion

--- components/admin/submissions/status/evaluation-table/index.js
+++ components/admin/submissions/status/evaluation-table/index.js
@@ -1,11 +1,15 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { withForm } from "../../../../../helpers/withForm"
 import Link from "next/link"
-import { List } from "antd"
+import { Divider, Typography, Row, Col } from "antd"
 import { EvaluationActionButtons } from "./action-buttons"
 
 function EvaluationTable({ data }) {
-  const [state, setState] = useState({ submissions: data })
+  const [state, setState] = useState({ submissions: [] })
+
+  useEffect(() => {
+    setState({ submissions: data })
+  }, [data])
 
   const onChange = id => {
     const newSubmissions = [...state.submissions].filter(e => e.id !== id)
@@ -13,16 +17,23 @@ function EvaluationTable({ data }) {
   }
 
   return (
-    <List bordered>
-      {state.submissions.map(({ id, name }) => (
-        <List.Item key={id}>
-          <Link href={`/admin/submissions/${id}`}>
-            <a>{name}</a>
-          </Link>
-          <EvaluationActionButtons id={id} onChange={onChange} />
-        </List.Item>
+    <Row justify="space-between" align="middle">
+      {state.submissions.map(({ id, name, technicalOpinion }) => (
+        <>
+          <Col span={20}>
+            <Link href={`/admin/submissions/${id}`}>
+              <a>{name}</a>
+            </Link>
+            <br />
+            <Typography.Text>{technicalOpinion}</Typography.Text>
+          </Col>
+          <Col>
+            <EvaluationActionButtons id={id} onChange={onChange} />
+          </Col>
+          <Divider />
+        </>
       ))}
-    </List>
+    </Row>
   )
 }
 
