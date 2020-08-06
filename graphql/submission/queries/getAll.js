e4185a28f5844f2474b12dea732adc6be4363086
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-06 12:06:14 -0600

    [#FZC-I368] Updates getAll submissions query

--- graphql/submission/queries/getAll.js
+++ graphql/submission/queries/getAll.js
@@ -2,8 +2,8 @@ import { gql } from "apollo-boost"
 
 export const getAll = gql`
   query getSubmissions(
-    $state: String!,
-    $status: String
+    $state: State!,
+    $status: Status
   ) {
     Submissions(
       state: $state,
@@ -18,89 +18,10 @@ export const getAll = gql`
       applyingCall
       township
       region
-      allies
-      implementationPlace
-      responsible
-      startDate
-      endDate
       strategicAxis
-      preventionLevel
-      scope
-      issueDescription
-      description
-      justification
-      developmentObjective
-      generalObjective
-      specificObjectives {
-        description
-        indicators {
-          type
-          title
-          description
-          methodology
-          formula
-          meansOfVerification
-          baseline
-          goal
-          startDate
-          endDate
-          measurementPeriodicity
-          products
-          generalObjectiveSubmission {
-            id
-          }
-          developmentObjectiveSubmission {
-            id
-          }
-        }
-        activities {
-          title
-          description
-          responsible
-          methodology
-          formula
-          meansOfVerification
-          baseline
-          goal
-          place
-          months
-          inputs
-          products
-          specificObjective {
-            id
-          }
-        }
-      }
-      beneficiaries {
-        description
-        number
-        gender
-        educationLevel
-        age
-        preventionLevel
-      }
-      consultants {
-        description
-        commercialName
-        commercialAddress
-        contactName
-        phone
-        rfc
-        fiscalAddress
-        fiscalPersonType
-        hadReceivedSupports
-        supports {
-          name
-          date
-          amount
-        }
-      }
-      developmentObjectiveIndicators {
-        id
-      }
-      generalObjectiveIndicators {
-        id
-      }
+      status
+      statusChangedAt
+      deadline
       concepts {
         name
         region
@@ -114,25 +35,7 @@ export const getAll = gql`
           type
           percentage
         }
-        humanResource {
-          position
-          name
-          tasks
-          overseer
-          hours
-          contractType
-          salary
-          benefits
-          taxes
-          total
-        }
       }
-      status
-      statusChangedAt
-      deadline
-      signedContractAt
-      agreementNumber
-      technicalOpinion
     }
   }
 `
