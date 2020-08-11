833a70f411e0db44c8078826b16c008f6f739410
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-11 17:33:28 -0600

    [#FZC-I348] Updates MonitoringObjectives to handle objectives state

--- components/admin/projects/monitoring/technical/tabs/objectives/index.js
+++ components/admin/projects/monitoring/technical/tabs/objectives/index.js
@@ -1,13 +1,20 @@
 import { SearchFieldPrimary } from "../../../../../../shared"
 import { Alert } from "antd"
+import { useState } from "react"
 import { ObjectivesSummary } from "./summary"
 import { ObjectivesList } from "./list"
 
 export function MonitoringObjectives({ data }) {
   const { Submission } = data || {}
+
+  const [state, setState] = useState({
+    selectedRows: [],
+    fulfilled: {}
+  })
+
   return (
     <div className="objectives" style={{ marginTop: "-2.5rem"}}>
-      <ObjectivesSummary />
+      <ObjectivesSummary data={Submission} fulfilled={state.fulfilled} />
       <SearchFieldPrimary style={{marginBottom: "1rem"}} />
       <Alert
         type="info"
@@ -15,7 +22,10 @@ export function MonitoringObjectives({ data }) {
         message="Selecciona las actividades que hayas realizado, espeficica
             cuántos participantes tuviste en cada una y cúantos beneficiarios
             atendiste de acuerdo a los objetivos específicos y generales" />
-      <ObjectivesList data={Submission} />
+      <ObjectivesList
+        data={Submission}
+        setState={setState}
+        state={state} />
     </div>
   )
 }
