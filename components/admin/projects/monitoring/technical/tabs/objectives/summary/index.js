1783a76264d01f3d72340e2831ff2e59de47a557
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-11 17:32:57 -0600

    [#FZC-I348] Updates ObjectivesSummary to display real data

---
+++ components/admin/projects/monitoring/technical/tabs/objectives/summary/index.js
@@ -0,0 +1,39 @@
+import { Col, Divider, Row, Statistic } from "antd"
+import { Section } from "../../../../../../../shared/section"
+import {
+  decoratedGoal,
+  decoratedReal,
+  decoratedFulfilled
+} from "./data-decorator"
+
+export function ObjectivesSummary({ data, fulfilled }) {
+  const real = decoratedReal(fulfilled)
+  const goal = decoratedGoal(data)
+  const totalFulfilled = decoratedFulfilled(real, goal)
+  const color = parseInt(totalFulfilled) >= 80 ? "#008000" : "#cf1322"
+
+  return (
+    <Section style={{ margin: "1rem 0" }}>
+      <Row>
+        <Col flex="auto">
+          <Statistic title="Meta" value={goal} />
+        </Col>
+        <Col span={1} style={{textAlign: "center"}}>
+          <Divider type="vertical" />
+        </Col>
+        <Col flex="auto">
+          <Statistic title="Real" value={real} />
+        </Col>
+        <Col span={1} style={{textAlign: "center"}}>
+          <Divider type="vertical" />
+        </Col>
+        <Col flex="auto">
+          <Statistic
+            value={totalFulfilled}
+            title="Cumplimiento"
+            valueStyle={{ color: color }} />
+        </Col>
+      </Row>
+    </Section>
+  )
+}
