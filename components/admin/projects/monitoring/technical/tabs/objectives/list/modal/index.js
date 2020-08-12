8838bebeb7c5a820e4022e43e015c0d1268e3092
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-12 11:35:12 -0600

    [#FZC-I350] Updates ObjectivesModal to add heading section

--- components/admin/projects/monitoring/technical/tabs/objectives/list/modal/index.js
+++ components/admin/projects/monitoring/technical/tabs/objectives/list/modal/index.js
@@ -1,15 +1,14 @@
 import {
-  Divider,
-  Form,
-  Modal,
-  Popover,
-  Row
+  Col, Divider, Form, Modal,
+  Popover, Row, Tag, Typography
 } from "antd"
 import { ExclamationCircleOutlined } from "@ant-design/icons"
 import { useEffect } from "react"
+import { DateField } from "../../../../../../../../shared"
 import { ParticipantsText } from "./participants-text"
+import { getSelectValue } from "../../../../../../../../../helpers"
 
-export function ObjectivesModal({ edit, ...props }) {
+export function ObjectivesModal({ edit, onCancel, ...props }) {
   const [form] = Form.useForm()
 
   useEffect(() => {
@@ -27,26 +26,63 @@ export function ObjectivesModal({ edit, ...props }) {
     }
   }
 
+  const onClose = () => {
+    form.resetFields()
+    onCancel()
+  }
+
   return (
     <Modal
       cancelText="Cancelar"
       onOk={onOk}
+      width={550}
+      onCancel={onClose}
       okText="Guardar"
       {...props}>
       <Form
         form={form}
-        name="indicator-form"
-        layout="vertical">
+        name="indicator-form">
         <Row gutter={[10, 8]} justify="start">
-          <Divider plain orientation="left">Resultados</Divider>
-          <Divider plain orientation="left">
+          <Col span={24}>
+            <Tag color="gold">{edit?.title}</Tag>
+          </Col>
+          <Col span={24}>
+            <Typography.Text>{edit?.description}</Typography.Text>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              getValueFromEvent={getSelectValue}
+              style={{ marginBottom: "0" }}
+              name="dateApplied">
+              <DateField
+                bordered={false}
+                style={{ width: "15rem" }}
+                placeholder="Selecciona fecha de realización"
+                size="small"/>
+            </Form.Item>
+          </Col>
+          <Divider
+            plain
+            orientation="left"
+            style={{ margin: "10px 0" }}>
+            Resultados
+          </Divider>
+          <Divider
+            plain
+            orientation="left"
+            style={{ margin: "10px 0" }}>
             Participantes
             &nbsp;
             <Popover content={<ParticipantsText />}>
               <ExclamationCircleOutlined />
             </Popover>
           </Divider>
-          <Divider plain orientation="left">Indicador</Divider>
+          <Divider
+            plain
+            orientation="left"
+            style={{ margin: "10px 0" }}>
+            Indicador
+          </Divider>
         </Row>
       </Form>
     </Modal>
