9c9999ca8b8b768af1a96390f920e1071821ec09
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-14 12:55:50 -0600

    [#FZC-I350] Updates ObjectivesList

--- components/admin/projects/monitoring/technical/tabs/objectives/list/index.js
+++ components/admin/projects/monitoring/technical/tabs/objectives/list/index.js
@@ -1,15 +1,26 @@
-import { Button, InputNumber, Table } from "antd"
+import { Button, Table } from "antd"
 import { EditOutlined } from "@ant-design/icons"
 import { decoratedData } from "./data-decorator"
 import { ObjectivesModal } from "./modal"
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import {
   AdminSubmissionContext
 } from "../../../../../../../../contexts/admin/submissions/show"
 
-export function ObjectivesList({ data, state, setState }) {
+export function ObjectivesList({ data, completed, setCompleted }) {
+  const [state, setState] = useState({
+    selectedRows: [],
+    isModalOpen: false,
+    edit: undefined,
+    reports: []
+  })
+
+  useEffect(() => {
+    setState({ ...state, reports: data.technicalMonitoringReports })
+  }, [data])
+
   const dataSource = decoratedData(data)
-  const { save } = useContext(AdminSubmissionContext)
+  const { save, update, refetch } = useContext(AdminSubmissionContext)
 
   const onSelectChange = selectedRowKeys => {
     setState(prevState => (
@@ -17,30 +28,46 @@ export function ObjectivesList({ data, state, setState }) {
     ))
   }
 
-  const getFulfilled = row => {
-    const real = state.fulfilled[row.key] || 0
-    const goal = isNaN(parseInt(row.goal)) ? 1 : row.goal
-
-    const result = (real * 100) / goal
-    return `${result.toString().substring(0, 4)}%`
+  const getReport = row => {
+    const report = state.reports.find(r => r.key === row.key)
+    return report
   }
 
-  const handleChangeReal = (value, row) => {
-    const newFulfilled = { ...state.fulfilled }
-    newFulfilled[row.key] = value
+  const getReal = (row) => {
+    const report = state.reports.find(r => r.key === row.key)
+    return report?.completed || 0
+  }
 
-    setState(prevState => (
-      { ...prevState, fulfilled: newFulfilled }
-    ))
+  const getParticipants = row => {
+    const report = getReport(row)
+    const total = report?.participants?.reduce((acc, item) => (
+      acc += Number(item.amount || 0)
+    ), 0) || 0
+    return total
   }
 
   const onSave = monitoring => {
-    save(monitoring)
+    const newReports = [ ...state.reports ]
+
+    const index = newReports.findIndex(r => r.key === monitoring.key)
+    if (index >= 0){
+      newReports[index] = monitoring
+      update(monitoring)
+    } else {
+      newReports.push(monitoring)
+      save(monitoring)
+    }
+    setState({ ...state, reports: newReports })
+    refetch()
+
     onCancel()
   }
 
   const onEdit = row => {
-    setState({ ...state, isModalOpen: true, edit: row })
+    const report = state?.reports?.find(report =>
+      report.key === row.key
+    ) || {}
+    setState({ ...state, isModalOpen: true, edit: { ...row, ...report } })
   }
 
   const onCancel = () => {
@@ -75,20 +102,18 @@ export function ObjectivesList({ data, state, setState }) {
           dataIndex="goal"
           title="Meta" />
         <Table.Column
-          render={(t, row) =>
-            <InputNumber
-              defaultValue={0}
-              max={row.goal}
-              min={0}
-              onChange={value => handleChangeReal(value, row)} />
-          }
+          align="center"
+          render={(t, row) => getReal(row)}
           title="Real"
           width={90} />
         <Table.Column
           align="center"
-          render={(t, row) => getFulfilled(row)}
+          render={(t, row) => `${getReport(row)?.compliance || 0}%`}
           title="Cumplimiento" />
-        <Table.Column title="Participantes" />
+        <Table.Column
+          align="center"
+          render={(t, row) => getParticipants(row)}
+          title="Participantes" />
         <Table.Column
           render={(t, row) =>
             <Button
