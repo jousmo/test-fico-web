99bce92ed37f065c154f874eb242711f3ab30677
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-14 14:04:24 -0600

    [#FZC-I350] Updates MonitoringObjectives to remove unnecessary state

--- components/admin/projects/monitoring/technical/tabs/objectives/index.js
+++ components/admin/projects/monitoring/technical/tabs/objectives/index.js
@@ -1,28 +1,22 @@
 import { SearchFieldPrimary } from "../../../../../../shared"
 import { Alert } from "antd"
-import { useState } from "react"
 import { ObjectivesSummary } from "./summary"
 import { ObjectivesList } from "./list"
 
 export function MonitoringObjectives({ data }) {
   const { Submission } = data || {}
 
-  const [completed, setCompleted] = useState({})
-
   return (
     <div className="objectives" style={{ marginTop: "-2.5rem"}}>
-      <ObjectivesSummary data={Submission} completed={completed} />
+      <ObjectivesSummary data={Submission} />
       <SearchFieldPrimary style={{marginBottom: "1rem"}} />
       <Alert
         type="info"
         showIcon
-        message="Selecciona las actividades que hayas realizado, espeficica
-            cuántos participantes tuviste en cada una y cúantos beneficiarios
-            atendiste de acuerdo a los objetivos específicos y generales" />
+        message="Especifica cuántos participantes fueron atendidos y adjunta la
+        evidencia necesaria en cada objetivo y actividad" />
       <ObjectivesList
-        data={Submission}
-        setCompleted={setCompleted}
-        completed={completed} />
+        data={Submission} />
     </div>
   )
 }
