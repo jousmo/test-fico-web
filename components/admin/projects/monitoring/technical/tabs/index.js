3a000145e991dee243c5b3640e855a21900c7828
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-17 09:13:38 -0600

    [#FZC-I349] Adds filter to tab section in TechnicalMonitoringTabs

--- components/admin/projects/monitoring/technical/tabs/index.js
+++ components/admin/projects/monitoring/technical/tabs/index.js
@@ -1,13 +1,29 @@
 import { Tabs } from "antd"
+import { useState } from "react"
 import { withForm } from "../../../../../../helpers"
 import { MonitoringObjectives } from "./objectives"
+import { DateField } from "../../../../../shared"
 import "./style.sass"
 
 function TechnicalMonitoringTabs({ data }) {
+  const [filterState, setFilterState] = useState([])
+
+  const onFilterChange = ({ currentTarget }) => {
+    const { value } = currentTarget
+    setFilterState(value)
+  }
+
   return (
-    <Tabs defaultActiveKey="1" className="fico technical-monitoring">
+    <Tabs
+      defaultActiveKey="1"
+      className="fico technical-monitoring"
+      tabBarExtraContent={
+        <DateField
+          onChange={onFilterChange}
+          range />
+      }>
       <Tabs.TabPane tab="Objetivos y actividades" key="1">
-        <MonitoringObjectives data={data} />
+        <MonitoringObjectives data={data} dateFilter={filterState} />
       </Tabs.TabPane>
       <Tabs.TabPane tab="Retos y obstáculos" key="2">
 
