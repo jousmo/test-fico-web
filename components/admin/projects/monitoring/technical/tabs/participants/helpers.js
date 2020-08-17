4326b23f2116e2c7dd8c804bb7de684556a69519
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-17 13:29:27 -0600

    [#FZC-I351] Adds MonitoringParticipants component with StatisticHeader

---
+++ components/admin/projects/monitoring/technical/tabs/participants/helpers.js
@@ -0,0 +1,7 @@
+export const getStatistics = () => {
+  return [
+    { title: "Asistentes", value: 90 },
+    { title: "Beneficiarios", value: 45 },
+    { title: "Participantes totales", value: 135 }
+  ]
+}
