d85eca72126cf4df75d5a2f9387ed0a1a2d9a298
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-18 11:06:36 -0600

    [#FZC-I352] Adds decoratedData helper

--- components/admin/projects/monitoring/technical/tabs/participants/helpers.js
+++ components/admin/projects/monitoring/technical/tabs/participants/helpers.js
@@ -5,3 +5,31 @@ export const getStatistics = () => {
     { title: "Participantes totales", value: 135 }
   ]
 }
+
+export const decoratedData = data => {
+  const { Submission: { technicalMonitoringReports: reports } } = data
+  const result = {
+    beneficiaries: {},
+    participants: {}
+  }
+
+  reports?.forEach(({ key, participants }) => {
+    participants.forEach(p => {
+      const { age } = p
+      if (p.type === "BENEFICIARY"){
+        if(!result.beneficiaries[age]){
+          result.beneficiaries[age] = []
+        }
+
+        result.beneficiaries[age].push({key, ...p})
+      } else {
+        if(!result.participants[age]){
+          result.participants[age] = []
+        }
+
+        result.participants[age].push({key, ...p})
+      }
+    })
+  })
+  return result
+}
