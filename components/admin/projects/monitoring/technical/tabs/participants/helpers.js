35197f0c756e68e5f38a88c2edbb86f6385e7002
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-19 09:36:20 -0600

    [#FZC-I353] Updates MonitoringParticipants helpers to handle summary data

--- components/admin/projects/monitoring/technical/tabs/participants/helpers.js
+++ components/admin/projects/monitoring/technical/tabs/participants/helpers.js
@@ -1,8 +1,9 @@
-export const getStatistics = () => {
+export const getStatistics = values => {
+  const { beneficiaries, participants, total }= values
   return [
-    { title: "Asistentes", value: 90 },
-    { title: "Beneficiarios", value: 45 },
-    { title: "Participantes totales", value: 135 }
+    { title: "Asistentes", value: participants },
+    { title: "Beneficiarios", value: beneficiaries },
+    { title: "Participantes totales", value: total }
   ]
 }
 
@@ -18,25 +19,33 @@ export const decoratedData = data => {
       PRIMARY: {},
       SECONDARY: {},
       TERTIARY: {}
+    },
+    summary: {
+      beneficiaries: 0,
+      participants: 0,
+      total: 0
     }
   }
 
   reports?.forEach(({ key, participants }) => {
     participants.forEach(p => {
-      const { age, preventionLevel } = p
+      const { age, amount, preventionLevel } = p
       if (p.type === "BENEFICIARY"){
         if(!result.beneficiaries[preventionLevel][age]){
           result.beneficiaries[preventionLevel][age] = []
         }
 
+        result.summary.beneficiaries +=amount
         result.beneficiaries[preventionLevel][age].push({key, ...p})
       } else {
         if(!result.participants[preventionLevel][age]){
           result.participants[preventionLevel][age] = []
         }
 
+        result.summary.participants +=amount
         result.participants[preventionLevel][age].push({key, ...p})
       }
+      result.summary.total +=amount
     })
   })
   return result
