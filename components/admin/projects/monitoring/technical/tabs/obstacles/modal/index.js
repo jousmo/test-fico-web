4ed983846966065b15bb396286bb3728cc03ed84
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-19 16:19:28 -0600

    [#FZC-I382] Adds ObstaclesModal component

---
+++ components/admin/projects/monitoring/technical/tabs/obstacles/modal/index.js
@@ -0,0 +1,79 @@
+import { Col, Form, Input, Modal, Row } from "antd"
+
+export function ObstaclesModal({ onCancel, onSave, ...props }){
+  const [form] = Form.useForm()
+
+  const onOk = async () => {
+    try {
+      const values = await form.getFieldsValue()
+
+      onSave(values)
+      form.resetFields()
+    }
+    catch(e) {
+      console.error(e)
+    }
+  }
+
+  const onCancelModal = () => {
+    form.resetFields()
+    onCancel && onCancel()
+  }
+
+  return (
+    <Modal
+      title="Actualización"
+      onOk={onOk}
+      onCancel={onCancelModal}
+      width={600}
+      okText="Guardar actualización"
+      cancelText="Cancelar"
+      {...props}>
+      <Form
+        form={form}
+        name="obstacles-form"
+        layout="vertical">
+        <Row gutter={[10, 8]} justify="start">
+          <Col span={24}>
+            Actualiza los campos según lo que se requiere para la proxima etapa
+            del proyecto
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="correctiveActions"
+              id="correctiveActions"
+              label="Acciones correctivas"
+              style={{display: "inline"}}>
+              <Input.TextArea
+                placeholder="Describe las acciones correctivas"
+                rows={5} />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="challenges"
+              id="challenges"
+              label="Retos"
+              style={{display: "inline"}}>
+              <Input.TextArea
+                placeholder="Describe los retos a los cuales te enfrentarás"
+                rows={5} />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="obstacles"
+              id="obstacles"
+              label="Obstáculos"
+              style={{display: "inline"}}>
+              <Input.TextArea
+                placeholder="Describe los obstaculos que te impedirían lograr
+                tus objetivos de proyecto"
+                rows={5} />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
+  )
+}
