87e2addff61a2ce4564c454f5f1203a3a6104bc1
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-20 13:26:45 -0600

    [#FZC-I382] Updates MonitoringObstacles component

--- components/admin/projects/monitoring/technical/tabs/obstacles/index.js
+++ components/admin/projects/monitoring/technical/tabs/obstacles/index.js
@@ -1,7 +1,82 @@
-import { Card } from "antd"
+import { Card, Table } from "antd"
+import { useContext, useState } from "react"
+import { CompositeField } from "../../../../../../shared"
+import { ObstaclesModal } from "./modal"
+import moment from "moment"
+import {
+  AdminSubmissionContext
+} from "../../../../../../../contexts/admin/submissions/show"
 
-export function MonitoringObstacles(){
+export function MonitoringObstacles({ data = {} }){
+  const { updateSubmission } = useContext(AdminSubmissionContext)
+
+  const { Submission } = data
+
+  const [state, setState] = useState({
+    isModalOpen: false,
+    edit: undefined
+  })
+
+  const onClickAdd = () => {
+    setState({ isModalOpen: true })
+  }
+
+  const onCancel = () => {
+    setState({ isModalOpen: false, edit: undefined })
+  }
+
+  const onSave = (addNew, replaceItemAtIndex) => values => {
+    if(typeof values.index === "undefined") {
+      addNew(values)
+    }
+    else {
+      const index = values.index
+      delete values.index
+
+      replaceItemAtIndex(index, values)
+    }
+    onCancel()
+  }
+
+  const onEdit = (data, index) => () => {
+    data.index = index
+    setState({ isModalOpen: true, edit: data })
+  }
+
+  const onChange = items => {
+    updateSubmission({ technicalUpdates: items })
+  }
+
   return (
-    <Card className="obstacles"></Card>
+    <Card className="obstacles">
+      <CompositeField
+        onChange={onChange}
+        defaultValue={Submission?.technicalUpdates}
+        onClickAdd={onClickAdd}
+        orientation="TOP"
+        addLabel="Agregar actividad">
+        {({ items, addNew, replaceItemAtIndex }) =>
+          <div>
+            <ObstaclesModal
+              onCancel={onCancel}
+              onSave={onSave(addNew, replaceItemAtIndex)}
+              visible={state.isModalOpen}
+              edit={state.edit} />
+            <Table dataSource={items} pagination={false}>
+              <Table.Column
+                render={(t, {createdAt}) =>
+                  `Actualizado el ${moment(createdAt).format("DD/MM/YYYY")}`
+                }
+                title="Actualización" />
+              <Table.Column
+                render={(t, row, index) =>
+                  <a href="#" onClick={onEdit(row, index)}>Editar</a>
+                }
+                width={2} />
+            </Table>
+          </div>
+        }
+      </CompositeField>
+    </Card>
   )
 }
