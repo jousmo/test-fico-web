93fa39655a9fb464fb2750d6d5cae2772ac5dc7e
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-20 18:20:48 -0600

    [#FZC-I354] Updates ProjectClosureForm to adjust to new design

--- components/admin/projects/show/closure/form/index.js
+++ components/admin/projects/show/closure/form/index.js
@@ -1,18 +1,47 @@
+import { Button, Form, Input, Space, Typography } from "antd"
+import { DownloadOutlined } from "@ant-design/icons/lib"
 import { withForm } from "../../../../../../helpers/withForm"
-import { Typography } from "antd"
 import { UploadButton } from "../../../../../shared/upload-button"
+import "./styles.sass"
 
 function ProjectClosureForm({ data }) {
+  const [form] = Form.useForm()
+
   return (
-    <div>
-      <Typography.Text>
-        Subir el documento firmado de cierre de proyecto en formato .PDF
-      </Typography.Text>
-      <br />
-      <UploadButton style={{ marginTop: "5px" }}>
-        Subir
-      </UploadButton>
-    </div>
+    <Form
+      form={form}
+      className="fico project closure"
+      layout="vertical">
+      <Space direction="vertical">
+        <Typography.Text>
+          Descarga la ficha técnica y envíalo al área juridica, recaba las firmas
+          y adjunta el documento firmado aquí.
+        </Typography.Text>
+        <Space>
+          <Button icon={<DownloadOutlined />}>Descargar ficha técnica</Button>
+          <Form.Item
+            id="closureDocument"
+            name="closureDocument">
+            <UploadButton>
+              Subir documento de cierre
+            </UploadButton>
+          </Form.Item>
+        </Space>
+        <Form.Item
+          label="Descripción de cierre de proyecto"
+          id="closureDescription"
+          name="closureDescription">
+          <Input.TextArea rows={1} />
+        </Form.Item>
+        <Form.Item className="submit">
+          <Button
+            danger
+            htmlType="submit">
+            Concluir proyecto
+          </Button>
+        </Form.Item>
+      </Space>
+    </Form>
   )
 }
 
