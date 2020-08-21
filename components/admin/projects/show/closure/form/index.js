6dc260ddfc5e9a92c4ef4c182e8cd26d3361ac29
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-21 10:28:49 -0600

    [#FZC-I354] Updates ProjectClosureForm to use ant grid

--- components/admin/projects/show/closure/form/index.js
+++ components/admin/projects/show/closure/form/index.js
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Space, Typography } from "antd"
+import { Button, Col, Form, Input, Row, Typography } from "antd"
 import { DownloadOutlined } from "@ant-design/icons/lib"
 import { withForm } from "../../../../../../helpers/withForm"
 import { UploadButton } from "../../../../../shared/upload-button"
@@ -12,13 +12,17 @@ function ProjectClosureForm({ data }) {
       form={form}
       className="fico project closure"
       layout="vertical">
-      <Space direction="vertical">
-        <Typography.Text>
-          Descarga la ficha técnica y envíalo al área juridica, recaba las firmas
-          y adjunta el documento firmado aquí.
-        </Typography.Text>
-        <Space>
+      <Row gutter={[10, 8]}>
+        <Col span={24}>
+          <Typography.Text>
+            Descarga la ficha técnica y envíalo al área juridica, recaba las firmas
+            y adjunta el documento firmado aquí.
+          </Typography.Text>
+        </Col>
+        <Col>
           <Button icon={<DownloadOutlined />}>Descargar ficha técnica</Button>
+        </Col>
+        <Col>
           <Form.Item
             id="closureDocument"
             name="closureDocument">
@@ -26,21 +30,25 @@ function ProjectClosureForm({ data }) {
               Subir documento de cierre
             </UploadButton>
           </Form.Item>
-        </Space>
-        <Form.Item
-          label="Descripción de cierre de proyecto"
-          id="closureDescription"
-          name="closureDescription">
-          <Input.TextArea placeholder="Describe" rows={1} />
-        </Form.Item>
-        <Form.Item className="submit">
-          <Button
-            danger
-            htmlType="submit">
-            Concluir proyecto
-          </Button>
-        </Form.Item>
-      </Space>
+        </Col>
+        <Col span={24}>
+          <Form.Item
+            label="Descripción de cierre de proyecto"
+            id="closureDescription"
+            name="closureDescription">
+            <Input.TextArea placeholder="Describe" rows={1} />
+          </Form.Item>
+        </Col>
+        <Col push={19}>
+          <Form.Item className="submit">
+            <Button
+              danger
+              htmlType="submit">
+              Concluir proyecto
+            </Button>
+          </Form.Item>
+        </Col>
+      </Row>
     </Form>
   )
 }
