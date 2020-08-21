26618074159d1c10b42e33b471cde3f9b93cb656
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-21 10:55:06 -0600

    [#FZC-I354] Adds ConfirmModal shared component

---
+++ components/shared/confirm-modal/index.js
@@ -0,0 +1,19 @@
+import { Col, Modal, Row, Typography } from "antd"
+import { ExclamationCircleTwoTone } from "@ant-design/icons/lib"
+
+export function ConfirmModal({ title, body, ...props }){
+  return (
+    <Modal
+      width={350}
+      {...props}>
+      <Row gutter={[10, 8]}>
+        <Col><ExclamationCircleTwoTone twoToneColor="#F9BD55" /></Col>
+        <Col>
+          <Typography.Text strong>{title}</Typography.Text>
+          <br />
+          <Typography.Text>{body}</Typography.Text>
+        </Col>
+      </Row>
+    </Modal>
+  )
+}
