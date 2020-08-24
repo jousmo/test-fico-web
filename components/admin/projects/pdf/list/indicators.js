63239a5ef45d74f5388e528a9c112f01cf35d3c3
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-24 16:48:52 -0600

    [#FZC-I355] Adds ProjectIndicators component

---
+++ components/admin/projects/pdf/list/indicators.js
@@ -0,0 +1,38 @@
+import { Col, Divider, Row, Typography } from "antd"
+
+export function ProjectIndicators({ data }){
+  const indicatorTypes = {
+    IE: "Indicador específico",
+    OD: "Indicador de desarrollo",
+    OG: "Indicador general",
+    AE: "Actividad",
+  }
+
+  const getIndicatorName = key => {
+    const [indicator, number] = key.split("_")
+    return `${indicatorTypes[indicator]} ${number}`
+  }
+
+  return (
+    <Row>
+      <Col span={12}>
+        <Typography.Text strong>Indicador de resultados final</Typography.Text>
+      </Col>
+      <Col span={8}>
+        <Typography.Text strong>Meta</Typography.Text>
+      </Col>
+      <Col span={4}>
+        <Typography.Text strong>Resultado</Typography.Text>
+      </Col>
+      <Divider />
+      {data?.technicalMonitoringReports?.map(report =>
+        <>
+          <Col span={12}>{getIndicatorName(report.key)}</Col>
+          <Col span={8}>{report.goal}</Col>
+          <Col span={4}>{report.completed}</Col>
+          <Divider />
+        </>
+      )}
+    </Row>
+  )
+}
