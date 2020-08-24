7c9a73a76a5e01f2fc60523d5ca4076a0accfe90
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-24 13:09:42 -0600

    [#FZC-I355] Updates ProjectPDF component to implement logo and PDFHeading

--- components/admin/projects/pdf/index.js
+++ components/admin/projects/pdf/index.js
@@ -1,9 +1,11 @@
+import PDFHeading from "./heading"
 import "./styles.sass"
 
 export function ProjectPDF(){
   return (
     <div className="fico pdf project">
-
+      <img alt="logo" src="/assets/redLogo.svg" />
+      <PDFHeading title="Cierre de proyecto" />
     </div>
   )
 }
