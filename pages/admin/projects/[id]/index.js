7682d0116ffe23efd343309f5624c8dde8d704d1
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-24 10:59:26 -0600

    [#FZC-I354] Updates Project page to add mutation

--- pages/admin/projects/[id]/index.js
+++ pages/admin/projects/[id]/index.js
@@ -13,26 +13,53 @@ import {
   AdminSubmissionContext
 } from "../../../../contexts/admin/submissions/show"
 import { submission } from "../../../../graphql/submission"
-import { useMemo, useState } from "react"
-import { useQuery } from "@apollo/react-hooks"
+import { useCallback, useMemo } from "react"
+import { useMutation, useQuery } from "@apollo/react-hooks"
 import { withApollo } from "../../../../helpers/withApollo"
 import { PageContext } from "../../../../contexts/page"
+import { loadingAlert, success, warning } from "../../../../helpers/alert"
 
 function Project({ client, query }) {
-  const [ state ] = useState({
-    submissionDetail: {}
-  })
+  const [updateSubmission] = useMutation(
+    submission.mutations.updateById, {
+      client: client,
+      awaitRefetchQueries: true,
+      refetchQueries: [
+        {
+          query: submission.queries.getById,
+          variables: { id: query.id }
+        }
+      ]
+    }
+  )
 
   const { loading, error, data } = useQuery(submission.queries.getById, {
     client: client,
     variables: { id: query.id }
   })
 
+  const save = useCallback(async submission => {
+    const saving = loadingAlert()
+
+    try {
+      await updateSubmission({
+        variables: { data: submission, id: query.id }
+      })
+      success()
+      saving()
+    }
+    catch(e) {
+      warning()
+      console.error(e)
+    }
+  }, [updateSubmission])
+
   const injectActions = useMemo(() => ({
     loading,
     error,
-    data
-  }), [state, loading])
+    data,
+    save
+  }), [loading])
 
   return (
     <PageContext.Provider
