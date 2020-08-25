835f324b03d095b38ecbd6556ed13b00d8a485da
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-25 15:28:55 -0600

    [#FZC-I357] Updates UploadTooltip to add readOnly prop

--- components/shared/upload-tooltip/index.js
+++ components/shared/upload-tooltip/index.js
@@ -2,13 +2,13 @@ import { Popover, Button } from "antd"
 import { UploadButton } from ".."
 import { PaperClipOutlined, UploadOutlined } from "@ant-design/icons"
 
-export function UploadTooltip({ body, small, title }){
+export function UploadTooltip({ body, small, title, readOnly }){
   const content = (
     <div style={{width: "300px"}}>
       <p>
         {body}
       </p>
-      <UploadButton>Adjuntar documento</UploadButton>
+      <UploadButton disabled={readOnly}>Adjuntar documento</UploadButton>
     </div>
   )
 
