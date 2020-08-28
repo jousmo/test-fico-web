05ee1caba9181b8df752015d67b493dbb31b47ac
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-28 11:45:53 -0600

    [#FZC-I385] Moves UploadButton FileInput to different path

---
+++ components/implementer/submissions/show/agreement-documents/form/fileInput.js
@@ -0,0 +1,21 @@
+import { List, Tooltip } from "antd"
+import { QuestionCircleFilled } from "@ant-design/icons"
+import { UploadButton } from "../../../../../shared"
+
+export function FileInput({label, helpText, ...props}) {
+  return (
+    <List.Item
+      actions={[
+        <UploadButton key="Upload-Button" {...props}>
+          Subir documento
+        </UploadButton>
+      ]}
+      style={{borderBottom: 0}}>
+      { helpText ?
+        <Tooltip title={helpText}>
+          {label} <QuestionCircleFilled style={{fontSize: "12px"}} />
+        </Tooltip>
+      : label }
+    </List.Item>
+  )
+}
