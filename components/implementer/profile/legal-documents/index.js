ee4604418d24166c0784f3061cc36dfe16fb46de
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-28 12:50:10 -0600

    [#FZC-I385] Updates LegalDocuments component to get context variables

--- components/implementer/profile/legal-documents/index.js
+++ components/implementer/profile/legal-documents/index.js
@@ -1,14 +1,27 @@
-import { Section } from "../../../shared";
-import { LegalDocumentsForm } from "./form";
-import { useContext } from "react";
-import { ImplementerProfileContext } from "../../../../contexts/implementer/profile";
+import { Section } from "../../../shared"
+import LegalDocumentsForm from "./form"
+import { useContext } from "react"
+import { ImplementerProfileContext } from "../../../../contexts/implementer/profile"
 
 export function LegalDocuments() {
-  const { isGovernment } = useContext(ImplementerProfileContext)
+  const {
+    data,
+    error,
+    loading,
+    addDocument,
+    isGovernment,
+    removeDocument
+  } = useContext(ImplementerProfileContext)
 
   return (
     <Section title="3. Documentos legales">
-      <LegalDocumentsForm isGovernment={isGovernment()} />
+      <LegalDocumentsForm
+        error={error}
+        isLoading={loading}
+        data={data?.Implementer}
+        addDocument={addDocument}
+        removeDocument={removeDocument}
+        isGovernment={isGovernment()} />
     </Section>
   )
 }
