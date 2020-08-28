40749dc8cb3cf0732fe4ce422b87c939e628f01c
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-08-28 12:50:56 -0600

    [#FZC-I385] Updates legal documents FileInput to use UploadButtonForm

--- components/implementer/profile/legal-documents/form/fileInput.js
+++ components/implementer/profile/legal-documents/form/fileInput.js
@@ -1,15 +1,24 @@
 import { List, Tooltip } from "antd"
-import { QuestionCircleFilled } from "@ant-design/icons";
-import { UploadButton } from "../../../../shared";
+import { QuestionCircleFilled } from "@ant-design/icons"
+import { UploadButtonForm } from "../../../../shared"
 
-export function FileInput({label, helpText, ...props}) {
+export function FileInputForm({label, helpText, ...props}) {
   return (
-    <List.Item actions={[<UploadButton {...props}>Subir documento</UploadButton>]} style={{borderBottom: 0}}>
+    <List.Item
+      actions={[
+        <UploadButtonForm
+          key="UploadButton"
+          maxFile={1}
+          {...props}>
+          Subir documento
+        </UploadButtonForm>
+      ]}
+      style={{borderBottom: 0}}>
       { helpText ?
         <Tooltip title={helpText}>
           {label} <QuestionCircleFilled style={{fontSize: "12px"}} />
         </Tooltip>
-      : label }
+        : label }
     </List.Item>
   )
 }
