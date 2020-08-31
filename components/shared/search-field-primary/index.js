519021d6673bce4befcbbf6f8d1dadfd52b2da1f
Jose Uscanga <jousmo@outlook.com>
2020-08-31 14:43:42 -0500

    [FZC-I397] Create search box in expenses

--- components/shared/search-field-primary/index.js
+++ components/shared/search-field-primary/index.js
@@ -1,9 +1,12 @@
 import { Form, Input } from "antd"
 
-export function SearchFieldPrimary({ style }) {
+export function SearchFieldPrimary({ style, onSearch = null }) {
   return (
     <Form style={{...style}}>
-      <Input.Search placeholder="Buscar" enterButton/>
+      <Input.Search
+        placeholder="Buscar"
+        onSearch={onSearch}
+        enterButton />
     </Form>
   )
 }
