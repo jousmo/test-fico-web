39d7da780da6645dc4d68f82a3ac659a1623aeae
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-04 13:19:55 -0600

    [#FZC-I406] Updates DownloadButton component to pass props to button

--- components/shared/download-button/index.js
+++ components/shared/download-button/index.js
@@ -1,12 +1,12 @@
 import { Button } from "antd"
 import { DownloadOutlined } from "@ant-design/icons"
 
-export function DownloadButton({children, outlined, style}) {
+export function DownloadButton({children, outlined, style, ...props}) {
   if (outlined){
     style = { backgroundColor: "#c68e8f", color: "white", ...style }
   }
   return (
-    <Button style={style}>
+    <Button style={style} {...props}>
       <DownloadOutlined /> {children}
     </Button>
   )
