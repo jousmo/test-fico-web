f4a2742fd19756ea137b61fd573403c049e2c2f4
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-07 15:26:30 -0600

    [#FZC-I407] Updates consultant comment helpers to remove unnecessary index

--- contexts/admin/submissions/review/comments/helpers/sections/consultant.js
+++ contexts/admin/submissions/review/comments/helpers/sections/consultant.js
@@ -1,25 +1,25 @@
-export const deleteConsultantComments = (submission, toDelete, index) => {
+export const deleteConsultantComments = (submission, toDelete, i) => {
   const consultants = [...submission?.consultants]
-  const consultant = consultants[index]
+  const { index, ...consultant } = consultants[i]
   const newComments = consultant?.comments?.filter(e =>
     (e.comment !== toDelete.comment && e.createdAt !== toDelete.createdAt)
   )
-  consultants[index] = {
+  consultants[i] = {
     ...consultant,
     comments: newComments
   }
   return consultants
 }
 
-export const addConsultantComment = (submission, comment, index) => {
+export const addConsultantComment = (submission, comment, i) => {
   const consultants = [...submission?.consultants]
-  const consultant = consultants[index]
+  const { index, ...consultant } = consultants[i]
   const consultantComments = consultant?.comments || []
   const newComments = [
     ...consultantComments,
     comment
   ]
-  consultants[index] = {
+  consultants[i] = {
     ...consultant,
     comments: newComments
   }
