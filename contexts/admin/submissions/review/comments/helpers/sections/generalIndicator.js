f9d67d06cf3fa32bb97f50ceeb81d5895f57f0ce
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-07 16:41:27 -0600

    [#FZC-I407] Updates general indicators comment helpers to remove unnecessary index

--- contexts/admin/submissions/review/comments/helpers/sections/generalIndicator.js
+++ contexts/admin/submissions/review/comments/helpers/sections/generalIndicator.js
@@ -1,25 +1,25 @@
-export const deleteGeneralIComments = (submission, toDelete, index) => {
+export const deleteGeneralIComments = (submission, toDelete, i) => {
   const indicators = [...submission?.generalObjectiveIndicators]
-  const indicator = indicators[index]
+  const { index, ...indicator } = indicators[i]
   const newComments = indicator?.comments?.filter(e =>
     (e.comment !== toDelete.comment && e.createdAt !== toDelete.createdAt)
   )
-  indicators[index] = {
+  indicators[i] = {
     ...indicator,
     comments: newComments
   }
   return indicators
 }
 
-export const addGeneralIComment = (submission, comment, index) => {
+export const addGeneralIComment = (submission, comment, i) => {
   const indicators = [...submission?.generalObjectiveIndicators]
-  const indicator = indicators[index]
+  const { index, ...indicator } = indicators[i]
   const comments = indicator?.comments || []
   const newComments = [
     ...comments,
     comment
   ]
-  indicators[index] = {
+  indicators[i] = {
     ...indicator,
     comments: newComments
   }
