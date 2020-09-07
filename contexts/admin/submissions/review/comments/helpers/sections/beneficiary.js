ade1a81a60ae5f3e2d8534f5c553400f16c2c8c0
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-07 16:00:08 -0600

    [#FZC-I407] Updates beneficiary comment helpers to remove unnecessary index

--- contexts/admin/submissions/review/comments/helpers/sections/beneficiary.js
+++ contexts/admin/submissions/review/comments/helpers/sections/beneficiary.js
@@ -1,25 +1,25 @@
-export const deleteBeneficiaryComments = (submission, toDelete, index) => {
+export const deleteBeneficiaryComments = (submission, toDelete, i) => {
   const beneficiaries = [...submission?.beneficiaries]
-  const beneficiary = beneficiaries[index]
+  const { index, ...beneficiary } = beneficiaries[i]
   const newComments = [...beneficiary?.comments].filter(e =>
     (e.comment !== toDelete.comment && e.createdAt !== toDelete.createdAt)
   )
-  beneficiaries[index] = {
+  beneficiaries[i] = {
     ...beneficiary,
     comments: newComments
   }
   return beneficiaries
 }
 
-export const addBeneficiaryComment = (submission, comment, index) => {
+export const addBeneficiaryComment = (submission, comment, i) => {
   const beneficiaries = [...submission?.beneficiaries]
-  const beneficiary = beneficiaries[index]
+  const { index, ...beneficiary } = beneficiaries[i]
   const comments = beneficiary?.comments || []
   const newComments = [
     ...comments,
     comment
   ]
-  beneficiaries[index] = {
+  beneficiaries[i] = {
     ...beneficiary,
     comments: newComments
   }
