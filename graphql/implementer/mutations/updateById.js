b41e21581d4e3106885703ce522f4d734a20e2c0
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-09 15:55:22 -0600

    [#FZC-I301] Updates implementer page and query/mutation to use context token

--- graphql/implementer/mutations/updateById.js
+++ graphql/implementer/mutations/updateById.js
@@ -1,11 +1,8 @@
 import { gql } from "apollo-boost"
 
 export const updateById = gql`
-  mutation UpdateImplementerById(
-    $id: ID!,
-    $data: CreateImplementer!
-  ){
-    UpdateImplementer(id: $id, data: $data) {
+  mutation UpdateImplementerById($data: CreateImplementer!){
+    UpdateImplementer(data: $data) {
       id
     }
   }
