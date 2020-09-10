a51c5cad313f7b5f3600b431e532f5e7d960430c
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-10 17:28:36 -0600

    [#FZC-I551] Updates ProjectScheduleList component to add table columns

--- components/admin/projects/schedule/list/index.js
+++ components/admin/projects/schedule/list/index.js
@@ -1,5 +1,7 @@
 import { withForm } from "../../../../../helpers/withForm"
-import { Table } from "antd"
+import { Button, Table } from "antd"
+import { CheckSquareTwoTone, EditOutlined } from "@ant-design/icons"
+import { Tooltip } from "../../../../shared/tooltip"
 
 function ProjectScheduleList({ data }) {
   const dataSource = []
@@ -9,6 +11,35 @@ function ProjectScheduleList({ data }) {
         className="activities-list"
         dataSource={dataSource}
         size="middle">
+        <Table.Column
+          render={(t, row) => <CheckSquareTwoTone />}
+          title={<CheckSquareTwoTone />}/>
+        <Table.Column
+          dataIndex="level"
+          title="Nivel" />
+        <Table.Column
+          title="Resumen narrativo"
+          render={text => <Tooltip value={text || ""}/>}
+          sorter={(a, b) => a.description?.localeCompare(b.description)}
+          showSorterTooltip={false}
+          dataIndex="description" />
+          <Table.Column
+            title="Fecha y hora"
+            sorter={(a, b) => a.date?.localeCompare(b.date)}
+            showSorterTooltip={false}
+            dataIndex="date" />
+          <Table.Column
+            title="Lugar"
+            sorter={(a, b) => a.location?.localeCompare(b.location)}
+            showSorterTooltip={false}
+            dataIndex="location" />
+        <Table.Column
+          render={(t, row) =>
+            <Button
+              icon={<EditOutlined />}
+              onClick={() => null}
+              shape="circle" />
+          } />
       </Table>
     </>
   )
