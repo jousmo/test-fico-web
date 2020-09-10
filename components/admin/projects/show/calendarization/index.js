5973c9d745ba22450cb0eedecd410ad9d4a8e135
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-10 12:30:11 -0600

    [#FZC-I551] Updates Calendarization component

--- components/admin/projects/show/calendarization/index.js
+++ components/admin/projects/show/calendarization/index.js
@@ -1,9 +1,23 @@
 import { Section } from "../../../../shared"
+import { useContext } from "react"
+import {
+  AdminSubmissionContext
+} from "../../../../../contexts/admin/submissions/show"
+import CalendarizationList from "./list"
 
 export function Calendarization(){
+  const {
+    loading,
+    error,
+    data
+  } = useContext(AdminSubmissionContext)
+
   return (
     <Section title="Calendarización de actividades">
-
+      <CalendarizationList
+        data={data}
+        error={error}
+        isLoading={loading} />
     </Section>
   )
 }
