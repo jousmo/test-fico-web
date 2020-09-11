dbe1c3e39437836ee27f8fcbe32455f6e1ad7ccf
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-11 16:24:19 -0600

    [#FZC-I552] Removes indicators from datasource in data decorator

--- components/admin/projects/schedule/list/data-decorator.js
+++ components/admin/projects/schedule/list/data-decorator.js
@@ -5,16 +5,6 @@ export const decoratedData = (data) => {
   data?.Submission?.specificObjectives?.forEach((objective, index) => {
     const objNumber = index + 1
 
-    objective.indicators.forEach((indicator, i) => {
-      const key = `I${objNumber}.${i + 1}`
-      result.push({
-        description: indicator.description,
-        key,
-        level: <Tag color="processing">{key}</Tag>,
-        ...indicator
-      })
-    })
-
     objective.activities.forEach((activity, i) => {
       const key = `A${objNumber}.${i + 1}`
       result.push({
