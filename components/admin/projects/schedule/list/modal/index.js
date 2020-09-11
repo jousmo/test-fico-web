6cbab2e6441c4887f4b4f9260c2fd2491a320f11
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-11 16:00:50 -0600

    [#FZC-I552] Finishes ScheduleModal component

--- components/admin/projects/schedule/list/modal/index.js
+++ components/admin/projects/schedule/list/modal/index.js
@@ -1,6 +1,7 @@
-import { Col, Form, Modal, Row, Tag, Typography } from "antd"
+import { Col, Divider, Form, Modal, Row, Tag, Typography } from "antd"
 import { useEffect } from "react"
 import { capitalize } from "lodash"
+import { ScheduleField } from "./schedule-field"
 import moment from "moment"
 moment.locale("es")
 
@@ -36,48 +37,65 @@ export function ScheduleModal({ edit, onCancel, onSave, ...props }) {
 
   return (
     <Modal
+      className="project schedule modal"
       destroyOnClose
       cancelText="Cancelar"
       onOk={onOk}
-      width={600}
+      width={650}
       onCancel={onClose}
       okText="Guardar"
       {...props}>
-      <Form
-        form={form}
-        name="indicator-form">
-        <Row gutter={[10, 8]} justify="start">
-          <Col span={24}>
-            <Tag color="processing">{edit?.key}</Tag>
-          </Col>
-          <Col span={24}>
-            <Typography.Text strong>{edit?.title}</Typography.Text>
-          </Col>
-          <Col span={24}>
-            <Typography.Text>{edit?.description}</Typography.Text>
-          </Col>
-          <Col span={24}>
-            <Typography.Text>Medio de verificación: </Typography.Text>
-            <Typography.Text strong>{edit?.meansOfVerification}</Typography.Text>
-          </Col>
-          <Col span={12}>
-            <Typography.Text>Linea base: </Typography.Text>
-            <Typography.Text strong>{edit?.baseline}</Typography.Text>
-          </Col>
-          <Col span={12}>
-            <Typography.Text>Meta: </Typography.Text>
-            <Typography.Text strong>{edit?.goal}</Typography.Text>
-          </Col>
-          <Col span={24}>
-            <Typography.Text>Meses de implementación: </Typography.Text>
-            <Typography.Text strong>
-              {edit?.months.map(range => (
-                `${getDate(range[0])} - ${getDate(range[1])}`
-              )).join(" | ")}
-            </Typography.Text>
-          </Col>
-        </Row>
-      </Form>
+      <Row gutter={[10, 8]} justify="start">
+        <Col span={24}>
+          <Tag color="processing">{edit?.key}</Tag>
+        </Col>
+        <Col span={24}>
+          <Typography.Text strong>{edit?.title}</Typography.Text>
+        </Col>
+        <Col span={24}>
+          <Typography.Text>{edit?.description}</Typography.Text>
+        </Col>
+        <Col span={24}>
+          <Typography.Text strong>Medio de verificación: </Typography.Text>
+          <Typography.Text>{edit?.meansOfVerification}</Typography.Text>
+        </Col>
+        <Col span={12}>
+          <Typography.Text strong>Linea base: </Typography.Text>
+          <Typography.Text>{edit?.baseline}</Typography.Text>
+        </Col>
+        <Col span={12}>
+          <Typography.Text strong>Meta: </Typography.Text>
+          <Typography.Text>{edit?.goal}</Typography.Text>
+        </Col>
+        <Col span={24}>
+          <Typography.Text strong>Meses de implementación: </Typography.Text>
+          <Typography.Text>
+            {edit?.months.map(range => (
+              `${getDate(range[0])} - ${getDate(range[1])}`
+            )).join(" | ")}
+          </Typography.Text>
+        </Col>
+        <Divider
+          orientation="left"
+          style={{ margin: "10px 0" }}>
+          Implementación
+        </Divider>
+        <Col span={24}>
+          <Typography.Text>
+            Si la actividad tiene más de un evento, agrega las fechas y el lugar donde se
+            realizará.
+          </Typography.Text>
+        </Col>
+        <Col span={24}>
+          <Form
+            form={form}
+            name="activities-schedule-form">
+            <Form.Item name="activitySchedule">
+              <ScheduleField />
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </Modal>
   )
 }
