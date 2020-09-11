bb38c890e8b56cc3bf3d12e91b423d31df0d2bf3
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-11 09:53:07 -0600

    [#FZC-I552] Implements ScheduleModal component

--- components/admin/projects/schedule/list/index.js
+++ components/admin/projects/schedule/list/index.js
@@ -2,13 +2,28 @@ import { withForm } from "../../../../../helpers/withForm"
 import { Button, Table } from "antd"
 import { CheckSquareTwoTone, EditOutlined } from "@ant-design/icons"
 import { Tooltip } from "../../../../shared/tooltip"
+import { useState } from "react"
+import { ScheduleModal } from "./modal"
 import { decoratedData } from "./data-decorator"
 
 function ProjectScheduleList({ data }) {
+  const [state, setState] = useState({ isModalOpen: false, edit: undefined })
   const dataSource = decoratedData(data)
+
+  const onEdit = row => {
+    setState({ isModalOpen: true, edit: row })
+  }
+
+  const onCancel = () => {
+    setState({ isModalOpen: false, edit: undefined })
+  }
 
   return (
     <>
+      <ScheduleModal
+        edit={state.edit}
+        onCancel={onCancel}
+        visible={state.isModalOpen} />
       <Table
         className="activities-list"
         dataSource={dataSource}
@@ -39,7 +54,7 @@ function ProjectScheduleList({ data }) {
           render={(t, row) =>
             <Button
               icon={<EditOutlined />}
-              onClick={() => null}
+              onClick={() => onEdit(row)}
               shape="circle" />
           } />
       </Table>
