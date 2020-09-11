f22bf05ff6b8b50bf91ba82f4a271458c05b2fe1
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-11 12:38:55 -0600

    [#FZC-I552] Finishes first section of ScheduleModal component

--- components/admin/projects/schedule/list/modal/index.js
+++ components/admin/projects/schedule/list/modal/index.js
@@ -1,5 +1,8 @@
-import { Form, Modal, Row } from "antd"
+import { Col, Form, Modal, Row, Tag, Typography } from "antd"
 import { useEffect } from "react"
+import { capitalize } from "lodash"
+import moment from "moment"
+moment.locale("es")
 
 export function ScheduleModal({ edit, onCancel, onSave, ...props }) {
   const [form] = Form.useForm()
@@ -27,6 +30,10 @@ export function ScheduleModal({ edit, onCancel, onSave, ...props }) {
     onCancel()
   }
 
+  const getDate = date => {
+    return capitalize(moment(date).format("MMMM YYYY"))
+  }
+
   return (
     <Modal
       destroyOnClose
@@ -40,6 +47,35 @@ export function ScheduleModal({ edit, onCancel, onSave, ...props }) {
         form={form}
         name="indicator-form">
         <Row gutter={[10, 8]} justify="start">
+          <Col span={24}>
+            <Tag color="processing">{edit?.key}</Tag>
+          </Col>
+          <Col span={24}>
+            <Typography.Text strong>{edit?.title}</Typography.Text>
+          </Col>
+          <Col span={24}>
+            <Typography.Text>{edit?.description}</Typography.Text>
+          </Col>
+          <Col span={24}>
+            <Typography.Text>Medio de verificación: </Typography.Text>
+            <Typography.Text strong>{edit?.meansOfVerification}</Typography.Text>
+          </Col>
+          <Col span={12}>
+            <Typography.Text>Linea base: </Typography.Text>
+            <Typography.Text strong>{edit?.baseline}</Typography.Text>
+          </Col>
+          <Col span={12}>
+            <Typography.Text>Meta: </Typography.Text>
+            <Typography.Text strong>{edit?.goal}</Typography.Text>
+          </Col>
+          <Col span={24}>
+            <Typography.Text>Meses de implementación: </Typography.Text>
+            <Typography.Text strong>
+              {edit?.months.map(range => (
+                `${getDate(range[0])} - ${getDate(range[1])}`
+              )).join(" | ")}
+            </Typography.Text>
+          </Col>
         </Row>
       </Form>
     </Modal>
