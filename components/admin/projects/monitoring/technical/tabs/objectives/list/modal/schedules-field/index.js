672e0bee58a01eea239a88d7d6396c2ce86a4b7f
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-17 14:16:03 -0600

    [#FZC-I554] Adds technical monitoring SchedulesField component

---
+++ components/admin/projects/monitoring/technical/tabs/objectives/list/modal/schedules-field/index.js
@@ -0,0 +1,46 @@
+import { Col, Form, Row } from "antd"
+import { CompositeField, DateField } from "../../../../../../../../../shared"
+import moment from "moment"
+
+export function SchedulesField({ ...props }) {
+  return (
+    <CompositeField
+      isAddDisabled
+      {...props}>
+      {({ items, updateItem }) =>
+        <Row>
+          { items.map((item, index) =>
+            <>
+              <Col span={10} style={{ padding: "0 5px"}}>
+                <Form.Item
+                  label="Fecha planeada"
+                  style={{ marginBottom: "0" }}>
+                  {moment(item.scheduledAt).format("DD/MM/YYYY")}
+                </Form.Item>
+              </Col>
+              <Col span={8} style={{ padding: "0 5px"}}>
+                <Form.Item
+                  label="Lugar"
+                  style={{ marginBottom: "0" }}>
+                  {item.place}
+                </Form.Item>
+              </Col>
+              <Col span={6} style={{ padding: "0 5px"}}>
+                <Form.Item
+                  style={{ marginBottom: "0" }}>
+                  <DateField
+                    id="completedAt"
+                    bordered={false}
+                    onChange={updateItem(index)}
+                    defaultValue={item.completedAt}
+                    placeholder="Realizado"
+                    size="small" />
+                </Form.Item>
+              </Col>
+            </>
+          ) }
+        </Row>
+      }
+    </CompositeField>
+  )
+}
