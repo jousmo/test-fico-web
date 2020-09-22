a8fbfeef271f79a13a6364fd2b097ead30c3f2ee
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-22 14:56:20 -0600

    [#FZC-I554] Updates Schedule decoratedData to sort activities by orderIndex

--- components/admin/projects/schedule/list/data-decorator.js
+++ components/admin/projects/schedule/list/data-decorator.js
@@ -4,15 +4,16 @@ export const decoratedData = (data) => {
   const result = []
   data?.Submission?.specificObjectives?.sort((a, b) => a.orderIndex - b.orderIndex)
     .forEach(objective => {
-      objective.activities.forEach((activity, i) => {
-        const key = `A${objective.orderIndex}.${i + 1}`
-        result.push({
-          description: activity.description,
-          key,
-          level: <Tag color="processing">{key}</Tag>,
-          objective: objective.id,
-          ...activity
-        })
+      objective.activities.sort((a, b) => a.orderIndex - b.orderIndex)
+        .forEach((activity, i) => {
+          const key = `A${objective.orderIndex}.${activity.orderIndex}`
+          result.push({
+            description: activity.description,
+            key,
+            level: <Tag color="processing">{key}</Tag>,
+            objective: objective.id,
+            ...activity
+          })
       })
   })
   return result
