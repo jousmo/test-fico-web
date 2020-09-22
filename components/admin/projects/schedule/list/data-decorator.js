bd42c470ad4fa7d0b60912fac3ef33f4e5885128
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-22 12:30:43 -0600

    [#FZC-I554] Updates monitoring schedule decoratedData helper

--- components/admin/projects/schedule/list/data-decorator.js
+++ components/admin/projects/schedule/list/data-decorator.js
@@ -2,19 +2,18 @@ import { Tag } from "antd"
 
 export const decoratedData = (data) => {
   const result = []
-  data?.Submission?.specificObjectives?.forEach((objective, index) => {
-    const objNumber = index + 1
-
-    objective.activities.forEach((activity, i) => {
-      const key = `A${objNumber}.${i + 1}`
-      result.push({
-        description: activity.description,
-        key,
-        level: <Tag color="processing">{key}</Tag>,
-        objective: objective.id,
-        ...activity
+  data?.Submission?.specificObjectives?.sort((a, b) => a.orderIndex - b.orderIndex)
+    .forEach(objective => {
+      objective.activities.forEach((activity, i) => {
+        const key = `A${objective.orderIndex}.${i + 1}`
+        result.push({
+          description: activity.description,
+          key,
+          level: <Tag color="processing">{key}</Tag>,
+          objective: objective.id,
+          ...activity
+        })
       })
-    })
   })
   return result
 }
