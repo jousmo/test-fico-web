cdb189d51745f11e1cfd6b9aa743de970091367d
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-09-23 11:25:23 -0600

    [#FZC-I583] Updates auth context

--- contexts/auth/index.js
+++ contexts/auth/index.js
@@ -1,1 +1,36 @@
-export * from "./context"
+import React, { useState, useEffect, useContext } from "react"
+import nookies from "nookies"
+import { firebase } from "../../helpers/auth"
+
+const AuthContext = React.createContext({
+  user: null
+})
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    return firebase.auth().onIdTokenChanged(async user => {
+      if (!user) {
+        setUser(null)
+        nookies.set(undefined, "token", "", null)
+        return
+      }
+      const { claims } = await user.getIdTokenResult()
+
+      const token = await user.getIdToken()
+      setUser({ ...user, claims })
+      nookies.set(undefined, "token", token, null)
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => {
+  return useContext(AuthContext)
+}
