b8b2fad25d4c5e178a69b17611eb64bd7428d25f
Alonso Gutierrez <alonso.gtb@gmail.com>
2020-10-02 10:41:04 -0600

    [#FZC-I590] Updates Tooltip shared component to return value if length does not exceed allowed length

--- components/shared/tooltip/index.js
+++ components/shared/tooltip/index.js
@@ -1,6 +1,10 @@
 import { Tooltip as ATooltip } from "antd"
 
 export function Tooltip({ value, length = 12 }){
+  if (value.length <= length) {
+    return value
+  }
+
   return (
     <ATooltip title={value}>
       {`${value.slice(0, length)}...`}
