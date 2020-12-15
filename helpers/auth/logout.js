8ca80f9176a9cdcb3f146d97b9bd538c460c0bac
Jose <jousmo@outlook.com>
2020-12-15 12:59:10 -0600

    [#FZC-I631] #comment Fix logout

--- helpers/auth/logout.js
+++ helpers/auth/logout.js
@@ -1,9 +1,11 @@
 import { message } from "antd"
 import { firebase } from "./index"
+import { destroyCookie } from "nookies"
 
 export async function Logout() {
   const loggingOut = message.loading("Cerrando sesión")
   await firebase.auth().signOut()
+  destroyCookie(null, "token")
   window.location.href = "/"
   loggingOut()
 }
