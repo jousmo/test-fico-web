7e0c04c2294e8bf314c55ca7fc6decd9bb718ebe
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-04 16:21:47 -0700

    [FZC-I676] Adds date range filter CensusTabs

--- components/admin/census/tabs/index.js
+++ components/admin/census/tabs/index.js
@@ -1,20 +1,32 @@
-import { Tabs } from "antd"
-import React from "react"
+import { DatePicker, Tabs } from "antd"
+import React, { useState } from "react"
 import "./style.sass"
-import { CensusBeneficiaries } from "../tabs/beneficiaries"
-import { CensusAssistants } from "../tabs/assistants"
+import { CensusBeneficiaries } from "./beneficiaries"
+import { CensusAssistants } from "./assistants"
 import { withForm } from "../../../../helpers"
 
 function CensusTabs({ data }) {
+  const [filterState, setFilterState] = useState([])
+
+  const onFilterChange = value => {
+    value = value && value?.map(v => v.format("YYYYMMDD"))
+    setFilterState(value)
+  }
+
   return (
     <Tabs
       className="fico census"
-      defaultActiveKey="1">
+      defaultActiveKey="1"
+      tabBarExtraContent={
+        <DatePicker.RangePicker
+          format="DD/MM/YYYY"
+          onChange={onFilterChange} />
+      }>
       <Tabs.TabPane tab="Beneficiarios" key="1">
-        <CensusBeneficiaries data={data?.beneficiaries}/>
+        <CensusBeneficiaries data={data?.beneficiaries} dateFilter={filterState} />
       </Tabs.TabPane>
       <Tabs.TabPane tab="Asistentes" key="2">
-        <CensusAssistants data={data?.assistants}/>
+        <CensusAssistants data={data?.assistants} dateFilter={filterState} />
       </Tabs.TabPane>
     </Tabs>
   )
