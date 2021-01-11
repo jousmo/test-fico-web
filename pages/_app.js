241386d9d5238270ab853b7f82323fe133f09336
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-11 16:01:44 -0700

    [FZC-I704] Implements Bugsnag ErrorBoundary in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import "antd/dist/antd.css"
 import { ConfigProvider } from "antd"
 import { antdConfig } from "../antd.config"
 import { AuthProvider } from "../contexts/auth"
+import { ErrorBoundary } from "../helpers"
 
 export default function App({ Component, pageProps }) {
   return (
     <ConfigProvider {...antdConfig}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <ErrorBoundary>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ErrorBoundary>
     </ConfigProvider>
   )
 }
