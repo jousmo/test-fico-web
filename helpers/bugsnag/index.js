2055e88ea9b826035239220cba8f87ae978bed21
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-14 11:12:45 -0700

    [FZC-I704] Updates Bugsnag.start to add new ignored error and set user to error

--- helpers/bugsnag/index.js
+++ helpers/bugsnag/index.js
@@ -1,21 +1,26 @@
 import React from "react"
 import Bugsnag from "@bugsnag/js"
 import BugsnagPluginReact from "@bugsnag/plugin-react"
+import { useAuth } from "../../contexts/auth"
 
 setTimeout(() => {}).__proto__.unref = () => {}
 
 if (!Bugsnag._client) {
   const ignoredErrors = [
-    "ResizeObserver loop limit exceeded"
+    "ResizeObserver loop limit exceeded",
+    "ResizeObserver loop completed with undelivered notifications."
   ]
 
   Bugsnag.start({
     apiKey: "aad08869c7acd9406928aec60a2baece",
     plugins: [new BugsnagPluginReact(React)],
-    onError: ({ errors: [{ errorMessage }]}) => {
+    onError: event => {
+      const { errors: [{ errorMessage }]} = event
       if(ignoredErrors.includes(errorMessage)) {
         return false
       }
+      const { user: { displayName, email } } = useAuth()
+      event.setUser(null, email, displayName)
     }
   })
 }
