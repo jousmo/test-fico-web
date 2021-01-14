9b2df6a1dc3d6098c6f72eef4bed80badeef5764
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-13 19:28:07 -0700

    [FZC-I704] Updates Bugsnag.start to add ignored errors

--- helpers/bugsnag/index.js
+++ helpers/bugsnag/index.js
@@ -5,9 +5,18 @@ import BugsnagPluginReact from "@bugsnag/plugin-react"
 setTimeout(() => {}).__proto__.unref = () => {}
 
 if (!Bugsnag._client) {
+  const ignoredErrors = [
+    "ResizeObserver loop limit exceeded"
+  ]
+
   Bugsnag.start({
     apiKey: "aad08869c7acd9406928aec60a2baece",
-    plugins: [new BugsnagPluginReact(React)]
+    plugins: [new BugsnagPluginReact(React)],
+    onError: ({ errors: [{ errorMessage }]}) => {
+      if(ignoredErrors.includes(errorMessage)) {
+        return false
+      }
+    }
   })
 }
 
