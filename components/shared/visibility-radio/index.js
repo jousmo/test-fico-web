27fb994debf48e1c59c2e656ca6b239013edde3f
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-15 15:08:10 -0700

    [FZC-I649] Updates VisibilityRadio to use value prop instead of defaultValue

--- components/shared/visibility-radio/index.js
+++ components/shared/visibility-radio/index.js
@@ -1,11 +1,11 @@
 import { Form, Radio } from "antd"
 
-export function VisibilityRadio({ children, label, onChange, visible = true }) {
+export function VisibilityRadio({ children, label, onChange, visible = false }) {
   return (
     <>
       <Form layout="vertical">
         <Form.Item label={label}>
-          <Radio.Group defaultValue={visible} onChange={({ target: { value }}) => onChange(value)}>
+          <Radio.Group value={visible} onChange={({ target: { value }}) => onChange(value)}>
             <Radio value={true}>Si</Radio>
             <Radio value={false}>No</Radio>
           </Radio.Group>
