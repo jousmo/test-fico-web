cc8d602b6948a864c9e90a9d346973fb48bfbd71
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-15 16:15:28 -0700

    [FZC-I719] Updates conceptTypes to add new values

--- helpers/selectOptions/implementer/submission/conceptTypes.js
+++ helpers/selectOptions/implementer/submission/conceptTypes.js
@@ -6,9 +6,11 @@ export const conceptTypes = [
   { label: "Gastos administrativos", value: "ADMINISTRATIVE_EXPENSES" },
   { label: "Gastos de viaje", value: "TRAVEL_EXPENSES" },
   { label: "Gastos financieros", value: "FINANCIAL_EXPENSES" },
+  { label: "Mantenimiento", value: "MAINTENANCE" },
   { label: "Materiales y suministros", value: "SUPPLIES" },
   { label: "Promoción y publicidad", value: "MARKETING" },
   { label: "Recurso humano", value: "HUMAN_RESOURCE" },
-  { label: "RH gasto administrativo", value: "ADMINISTRATIVE_HUMAN_RESOURCE" },
+  { label: "RH Gasto administrativo", value: "ADMINISTRATIVE_HUMAN_RESOURCE" },
+  { label: "RH Promoción y publicidad", value: "ADVERTISEMENT_HUMAN_RESOURCE" },
   { label: "Seguimiento y evaluación", value: "MONITORING" }
 ]
