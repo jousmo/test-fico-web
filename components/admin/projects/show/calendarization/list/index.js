7230ff14cf53d29cb5f70fad66d1f8d5fc7cad7c
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-19 14:24:12 -0700

    [FZC-I691] Updates CalendarizationList to remove unnecessary details

--- components/admin/projects/show/calendarization/list/index.js
+++ components/admin/projects/show/calendarization/list/index.js
@@ -1,30 +1,14 @@
-import { withForm } from "../../../../../../helpers"
 import { Button, List, Typography } from "antd"
 import { useRouter } from "next/router"
-import moment from "moment"
 
-function CalendarizationList({ data }){
+function CalendarizationList(){
   const router = useRouter() || {}
   const { query } = router || {}
-
-  const latestUpdated = data?.Submission?.specificObjectives?.reduce((result, objective) => {
-    objective.activities?.forEach(activity => {
-      activity.schedules?.map(({ scheduledAt }) => {
-        if (scheduledAt > result){
-          result = scheduledAt
-        }
-      })
-    })
-    return result
-  }, "")
 
   const userType = router?.route.includes("admin") ? "admin" : "implementer"
 
   return (
     <List>
-      <Typography.Text>
-        Agrega las actividades y eventos que sucederán durante tu próximo periodo de trabajo.
-      </Typography.Text>
       <List.Item
         actions={[
           <Button
@@ -33,13 +17,12 @@ function CalendarizationList({ data }){
             Actualizar
           </Button>
         ]}>
-        <Typography.Text strong>
-          Fecha de ultima actividad agendada
-          {latestUpdated !== "" && moment(latestUpdated).format("DD/MM/YYYY")}
+        <Typography.Text>
+          Agrega las actividades y eventos que sucederán durante tu próximo periodo de trabajo.
         </Typography.Text>
       </List.Item>
     </List>
   )
 }
 
-export default withForm(CalendarizationList)
+export default CalendarizationList
