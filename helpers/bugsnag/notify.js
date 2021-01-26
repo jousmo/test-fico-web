c17da24bf173b3d0051960c0fb72121296e7d9fa
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-26 10:55:35 -0700

    [#FZC-I740] Adds apolloError notify helper to handle GraphQL errors on Bugsnag

---
+++ helpers/bugsnag/notify.js
@@ -0,0 +1,18 @@
+import { Bugsnag } from "./index"
+import { warning } from "../alert"
+
+export const apolloError = err => {
+  const { graphQLErrors, networkError } = err
+  let messages = []
+
+  warning()
+  if (!!graphQLErrors.length) {
+    messages = graphQLErrors?.reduce((prev, current) => [...prev, current.message], [])
+  } else {
+    const { result: { errors } = {} } = networkError
+    messages = errors?.reduce((prev, current) => [...prev, current.message], [])
+  }
+
+  Bugsnag.notify(new Error(messages))
+  console.error(messages)
+}
