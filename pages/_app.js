47826e49af3040b62c51c7932b412e130f6a2cb6
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-27 11:02:27 -0700

    [#FZC-I740] Adds Head tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import "antd/dist/antd.css"
 import { ConfigProvider } from "antd"
 import { antdConfig } from "../antd.config"
@@ -8,6 +9,14 @@ export default function App({ Component, pageProps }) {
   return (
     <ConfigProvider {...antdConfig}>
       <ErrorBoundary>
+        <Head>
+          <title>Ficosec Proyectos</title>
+          <meta name="title" content="Ficosec Proyectos" />
+          <meta
+            name="description"
+            content={`Fondo de financiamiento de proyectos para la prevención y fortalecimiento de instituciones de
+            seguridad y justicia del empresariado chihuahuense.`} />
+        </Head>
         <AuthProvider>
           <Component {...pageProps} />
         </AuthProvider>
