f8095f8319ac8e99a8ad48e21bc5ca48c46ccbb1
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-01-28 14:47:28 -0700

    [#FZC-I735] Updates meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Head from "next/head"
 import "antd/dist/antd.css"
 import { ConfigProvider } from "antd"
 import { antdConfig } from "../antd.config"
--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,20 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <title>Ficosec Proyectos</title>
-          <meta name="title" content="Ficosec Proyectos" />
+          <meta property="og:title" content="Ficosec Proyectos" />
+          <meta property="og:site_name" content="FICOSEC.ORG" />
+          <meta property="og:locale" content="es_MX" />
+          <meta property="og:image" content="/assets/logo-fico.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="600" />
           <meta
-            name="description"
+            property="og:description"
             content={`Fondo de financiamiento de proyectos para la prevención y fortalecimiento de instituciones de
             seguridad y justicia del empresariado chihuahuense.`} />
+          <link
+            rel="icon"
+            type="image/svg"
+            href="/assets/icon.png" />
           <script src="/assets/vendors.js"></script>
         </Head>
         <body>
