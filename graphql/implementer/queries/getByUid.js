cc0124fb354bb38844d77701bc75fcc2823c0a37
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-02-10 12:57:33 -0700

    [#FZC-I766] Adds getByUid implementer query

---
+++ graphql/implementer/queries/getByUid.js
@@ -0,0 +1,48 @@
+import { gql } from "apollo-boost"
+
+export const getByUid = gql`
+  query ImplementerByUid($uid: String!) {
+    Implementer:ImplementerByUid(uid: $uid) {
+      id
+      type
+      name
+      director
+      rfc
+      commercialName
+      commercialAddress
+      fiscalAddress
+      phone
+      proofOfCharitableContributions
+      legalRepresentative
+      email
+      mission
+      vision
+      history
+      institutionalExperience
+      previousSupports
+      alliances
+      incomesAndExpenses
+      councilMembers {
+        name
+        charge
+        remuneration
+      }
+      documents {
+        id
+        name
+        type
+        url
+      }
+      projects {
+        name
+        objective
+        year
+        financing {
+          type
+          institution
+          amount
+        }
+      }
+    }
+  }
+`
