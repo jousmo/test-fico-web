e121a33b09fa728a50b1e3afc2be4fe203f3c3fc
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-02-15 17:53:44 -0700

    [#FZC-I694] Updates setReviewedComments

--- helpers/submissionFunctions/comments.js
+++ helpers/submissionFunctions/comments.js
@@ -1,7 +1,12 @@
-import { loadingAlert, success } from "../alert"
+import { loadingAlert, success, warning } from "../alert"
 import { apolloError } from "../bugsnag/notify"
 
 export const setReviewedComments = async (data, setState, updateComments) => {
+  if (!data.length) {
+    warning("Necesitas marcar mínimo un comentario")
+    return
+  }
+
   setState(state => ({ ...state, isSaving: true }))
   const saving = loadingAlert()
   try {
