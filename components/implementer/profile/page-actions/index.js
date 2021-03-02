f4cac3781c106c1e3046ae27aa84ffe3a1d438a0
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-03-02 12:44:49 -0700

    [#FZC-I797] Updates PageActions to clean up code

--- components/implementer/profile/page-actions/index.js
+++ components/implementer/profile/page-actions/index.js
@@ -1,11 +1,10 @@
 import { Button } from "antd"
 
-export function PageActions({save}) {
+export function PageActions({ save }) {
   return (
     <Button
-      onClick={() => { save() }}
-      type="primary"
-    >
+      onClick={save}
+      type="primary">
       Guardar
     </Button>
   )
