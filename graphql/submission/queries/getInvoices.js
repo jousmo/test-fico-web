fdd573d76d171b79582012e7a06e089301830607
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-03-03 13:07:22 -0700

    [#FZC-I726] Adds getInvoices query

---
+++ graphql/submission/queries/getInvoices.js
@@ -0,0 +1,58 @@
+import { gql } from "apollo-boost"
+
+export const getInvoices = gql`
+  query Invoices($id: ID!) {
+    Submission:Invoices(id: $id) {
+      status
+      endDate
+      budgeted
+      startDate
+      evidenced
+      difference
+      concepts {
+        id
+        type
+        name
+        index
+        region
+        budgeted
+        unitCost
+        totalUnits
+        measurementUnit
+        monthlyDistribution,
+        investmentDistribution {
+          id
+          name
+          type
+          percentage
+        }
+      }
+      invoices {
+        id
+        uuid
+        issuer
+        rfc
+        issuedAt
+        receptor
+        monthAt
+        reviewed
+        concept
+        category
+        paymentAt
+        ficosecPayment
+        investmentOnePayment
+        investmentTwoPayment
+        implementerPayment
+        amount
+        percentage
+        typeRH
+        documents {
+          id
+          type
+          name
+          url
+        }
+      }
+    }
+  }
+`
