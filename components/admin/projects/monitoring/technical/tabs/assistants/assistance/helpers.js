d233584e2ab53393562b5923d283920771ecbf84
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-03-26 10:51:41 -0700

    [#FZC-I817] Adds getAssistance helper

--- components/admin/projects/monitoring/technical/tabs/assistants/assistance/helpers.js
+++ components/admin/projects/monitoring/technical/tabs/assistants/assistance/helpers.js
@@ -4,3 +4,14 @@ export const getActivities = submission => {
     return prev
   }, [])
 }
+
+export const getAssistance = (assistance, assistants) => {
+  const { activities, assistanceAt } = assistance
+  const result = []
+  activities.forEach(activity => {
+    assistants.forEach(({ id: projectAssistant }) => {
+      result.push({ activity, projectAssistant, assistanceAt })
+    })
+  })
+  return result
+}
