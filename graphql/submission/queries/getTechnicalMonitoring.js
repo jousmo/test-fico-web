cdc208d2d384f343b5856e905ba096869a5f2db1
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-03-26 10:49:20 -0700

    [#FZC-I817] Adds getTechnicalMonitoring query

---
+++ graphql/submission/queries/getTechnicalMonitoring.js
@@ -0,0 +1,133 @@
+import { gql } from "apollo-boost"
+
+export const getTechnicalMonitoring = gql`
+  query TechnicalMonitoring($id: ID!) {
+    Submission:TechnicalMonitoring(id: $id) {
+      endDate
+      startDate
+      specificObjectives {
+        id
+        orderIndex
+        description
+        indicators {
+          id
+          goal
+          type
+          title
+          endDate
+          startDate
+          orderIndex
+          description
+          meansOfVerification
+        }
+        activities {
+          id
+          goal
+          title
+          orderIndex
+          description
+          responsible
+          meansOfVerification
+          schedules {
+            id
+            place
+            scheduledAt
+            completedAt
+          }
+        }
+      }
+      developmentObjectiveIndicators {
+        id
+        goal
+        type
+        title
+        endDate
+        startDate
+        methodology
+        description
+        meansOfVerification
+      }
+      generalObjectiveIndicators {
+        id
+        type
+        goal
+        title
+        endDate
+        startDate
+        description
+        meansOfVerification
+        measurementPeriodicity
+      }
+      state
+      status
+      technicalMonitoringReports{
+        id
+        key
+        goal
+        appliedAt
+        completed
+        compliance
+        reviewedAt
+        verificationDocuments{
+          id
+          name
+          url
+        }
+        participants{
+          id
+          age
+          type
+          amount
+          gender
+          preventionLevel
+        }
+      }
+      technicalUpdates{
+        id
+        createdAt
+        obstacles
+        challenges
+        correctiveActions
+      }
+      createdAt
+      assistants{
+        id
+        name
+        curp
+        phone
+        state
+        folio
+        gender
+        colony
+        lastName
+        birthdate
+        maidenName
+        beneficiary
+        municipality
+        assistance{
+          id
+          assistanceAt
+          activity {
+            id
+            description
+          }
+        }
+      }
+      projectBeneficiaries{
+        id
+        name
+        curp
+        phone
+        folio
+        state
+        gender
+        colony
+        lastName
+        birthdate
+        maidenName
+        municipality
+        projectAssistantId
+      }
+    }
+  }
+`
