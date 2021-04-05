38f96615bd0cef0540a7fd4b27f12d017026b35a
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-04-05 11:39:50 -0600

    [#FZC-I823] Adds budget export helpers

---
+++ components/implementer/submissions/new/technical-specification/budget/helpers.js
@@ -0,0 +1,98 @@
+import * as ExcelJS from "exceljs"
+import { saveAs } from "file-saver"
+import numeral from "numeral"
+import Moment from "moment"
+import { conceptTypes } from "../../../../../../helpers/selectOptions/implementer/submission"
+
+const displayAmount = (unitCost, value) =>
+  numeral(unitCost * Number(value || 0)).format("$0,0.00")
+
+const typeConcept = type => conceptTypes?.find(el => el.value === type)
+
+const projectMonths = ({ startDate,  endDate }) => Array
+  .from(
+    Moment
+      .range(Moment(startDate), Moment(endDate))
+      .by("month")
+  )
+
+const monthsColumns = months => {
+  const result = []
+  months?.forEach(r => {
+    result.push({ name: `Unidades ${Moment(r).format("MMMM YYYY")}` })
+    result.push({ name: `Costo ${Moment(r).format("MMMM YYYY")}` })
+  })
+  return result
+}
+
+export const exportBudget = async submission => {
+  const workbook = new ExcelJS.Workbook()
+  let worksheet = workbook.addWorksheet("Presupuesto")
+  let titleInfo = worksheet.getCell("A1")
+  titleInfo.value = "Presupuesto"
+  titleInfo.font = { size: 20, bold: true }
+
+
+  const months = projectMonths(submission)
+  const monthColumns = monthsColumns(months)
+  const investors = [
+    { name: "Implementadora" },
+    { name: "FICOSEC" }
+  ]
+  submission?.allies.forEach(ally => investors.push({ name: ally }))
+
+  const conceptsInfo = []
+  submission?.concepts?.forEach(concept => {
+    const {
+      id,
+      index,
+      monthlyDistribution,
+      investmentDistribution,
+      humanResource,
+      comments,
+      ...el
+    } = concept
+
+    el.unitCost = displayAmount(1, el?.unitCost)
+    el.budgeted = displayAmount(1, el?.budgeted)
+    el.type = typeConcept(el?.type)?.label
+    const result = Array.from(Object.values(el))
+
+    investmentDistribution?.forEach(el => {
+      result?.push(`${el?.percentage}%`)
+    })
+
+    months.forEach((month, index) => {
+      result.push(...[
+        monthlyDistribution?.[index],
+        displayAmount(concept?.unitCost, monthlyDistribution?.[index])
+      ])
+    })
+
+    conceptsInfo.push(result)
+  })
+
+  worksheet.addTable({
+    name: "Presupuesto",
+    ref: `A${worksheet?.lastRow?._number + 2}`,
+    headerRow: true,
+    style: {
+      showRowStripes: true,
+    },
+    columns: [
+      { name: "Concepto" },
+      { name: "Región" },
+      { name: "Tipo de gasto" },
+      { name: "Unidad de medida" },
+      { name: "Costo unitario" },
+      { name: "Total de unidades" },
+      { name: "Costo total" },
+      ...investors,
+      ...monthColumns
+    ],
+    rows: conceptsInfo
+  })
+
+  const buf = await workbook.xlsx.writeBuffer()
+  saveAs(new Blob([buf]), "Presupuesto.xlsx")
+}
