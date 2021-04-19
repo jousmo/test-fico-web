96438c041e0f3d61054a4fff4ded58186aabbe71
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-04-19 15:46:12 -0600

    [#FZC-I848] Updates SubmissionAttachments to add XLS attachments

--- components/admin/submissions/show/attachments/documents/index.js
+++ components/admin/submissions/show/attachments/documents/index.js
@@ -1,37 +1,12 @@
 import { withForm } from "../../../../../../helpers/withForm"
-import { Typography, Popover, Space } from "antd"
+import { Typography, Popover } from "antd"
 import { DownloadButton } from "../../../../../shared"
-import { useRouter } from "next/router"
-import Link from "next/link"
 import { submissionStatusOptions } from "../../../../../../helpers/selectOptions/shared/submission-status"
+import { AttachmentsPDFContent } from "./pdf-content"
+import { AttachmentsXLSContent } from "./xls-content"
 
 function SubmissionAttachments({ data }) {
-  const router = useRouter()
-  const url = `/admin/submissions/${router.query.id}/pdf`
   const statusIndex = submissionStatusOptions?.findIndex(e => e.value === data?.status)
-
-  const attachments = (
-    <Space direction="vertical">
-      <Link href={`${url}/general-information`}>
-        <a target="_blank">Información general</a>
-      </Link>
-      <Link href={`${url}/technical-specification`}>
-        <a target="_blank">Ficha técnica</a>
-      </Link>
-      <Link href={`${url}/budget`}>
-        <a target="_blank">Presupuesto</a>
-      </Link>
-      <Link href={`${url}/schedule`}>
-        <a target="_blank">Cronograma</a>
-      </Link>
-      <Link href={`${url}/reports`}>
-        <a target="_blank">Reportes</a>
-      </Link>
-      <Link href={`${url}/ministrations`}>
-        <a target="_blank">Ministraciones</a>
-      </Link>
-    </Space>
-  )
 
   return (
     <div>
@@ -41,11 +16,19 @@ function SubmissionAttachments({ data }) {
       </Typography.Text>
       <br />
       <Popover
-        content={statusIndex > 0 && attachments}
-        title="Anexos"
+        content={statusIndex > 0 && <AttachmentsPDFContent />}
+        title="Anexos PDF"
         trigger="click">
-        <DownloadButton outlined style={{margin: "5px"}}>
-          Descargar anexos
+        <DownloadButton outlined style={{ margin: "5px" }}>
+          Descargar anexos PDF
+        </DownloadButton>
+      </Popover>
+      <Popover
+        content={statusIndex > 0 && <AttachmentsXLSContent />}
+        title="Anexos XLS"
+        trigger="click">
+        <DownloadButton outlined style={{ margin: "5px" }}>
+          Descargar anexos XLS
         </DownloadButton>
       </Popover>
     </div>
