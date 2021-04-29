7e5065737c7546de7b26eb8ad122d384a552c85f
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-04-29 14:40:47 -0600

    [#FZC-I867] Updates technical monitoring assistance helpers to fix duplicated columns

--- components/admin/projects/monitoring/technical/tabs/assistance/helpers.js
+++ components/admin/projects/monitoring/technical/tabs/assistance/helpers.js
@@ -4,14 +4,14 @@ const addColumn = (activity, result, assistanceAt) => {
   if (!result[activity.id]) {
     result[activity.id] = { columns: new Set(), participants: {}, name: activity.title }
   }
-  result[activity.id].columns.add(assistanceAt)
+  result[activity.id].columns.add(moment(assistanceAt).format("MM/DD/YYYY HH:mm"))
 }
 
 const addParticipant = (activity, assistanceAt, assistantId, fullName, result) => {
   if (!result[activity].participants[assistantId]) {
     result[activity].participants[assistantId] = { assistantId, fullName, dates: [] }
   }
-  result[activity].participants[assistantId].dates.push(assistanceAt)
+  result[activity].participants[assistantId].dates.push(moment(assistanceAt).format("MM/DD/YYYY HH:mm"))
 }
 
 const getFullName = ({ lastName, name, maidenName }) => `${name} ${lastName} ${maidenName}`
