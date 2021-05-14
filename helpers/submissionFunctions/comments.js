74d3c0bcdc98bbd11bc5ea03d02eb3365d344309
Alonso Gutierrez <alonso.gtb@gmail.com>
2021-05-14 12:39:03 -0600

    [#FZC-I884] Updates submission helper functions to handle comments review

--- helpers/submissionFunctions/comments.js
+++ helpers/submissionFunctions/comments.js
@@ -1,23 +1,8 @@
-import { loadingAlert, success, warning } from "../alert"
+import { loadingAlert, success } from "../alert"
 import { apolloError } from "../bugsnag/notify"
 
-export const setReviewedComments = async (data, setState, updateComments) => {
-  if (!data.length) {
-    warning("Necesitas marcar mínimo un comentario")
-    return
-  }
-
-  setState(state => ({ ...state, isSaving: true }))
-  const saving = loadingAlert()
-  try {
-    await updateComments({ variables: { data } })
-    success()
-  }
-  catch(err) {
-    apolloError(err)
-  }
-  saving()
-  setState(state => ({ ...state, isSaving: false }))
+export const setReviewedComments = async (data, setState) => {
+  setState(state => ({ ...state, dirty: true, comments: [...state.comments || [], ...data] }))
 }
 
 export const setSave = async (comments, updateComments, id) => {
